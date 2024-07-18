8ae898d500e6854a6ba42f7563bd629a7fe31f77
monam2 <kangcw0107@gmail.com>
2024-07-19 03:31:39 +0900

    [FE] feat: 레이아웃 설정 파일 생성

---
+++ meetz-front/src/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+        {/* 헤더 */}
+        <main>
+            {children}
+        </main>
+        {/* 푸터 */}
+    </div>
+  );
+};
+
+export default Layout;
