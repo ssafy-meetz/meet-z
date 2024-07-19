f4580268efe7e7dafefa23618f355ca7320f0289
서민수 <minsu9812@naver.com>
2024-07-19 15:37:45 +0900

    [FE] feat: login 페이지 UI 퍼블리싱(#11)

--- meetz-front/src/view/login/login.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Login = () => {
-    return (
-        <div className='text-[--color-primary] font-[Pretendard] text-3xl font-extrabold'>Login</div>
-    )
-}
-
-export default Login
