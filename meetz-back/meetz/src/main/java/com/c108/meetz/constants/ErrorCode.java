dbc0571850b1ba4766f04eb450963f24e49e6c67
손다인 <dain0826@naver.com>
2024-07-19 17:56:38 +0900

    [BE] feat: 에러코드 상수 ㅌ추가 (#9)

--- meetz-back/meetz/src/main/java/com/c108/meetz/constants/ErrorCode.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/constants/ErrorCode.java
@@ -5,13 +5,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
 
     //400 BAD_REQUEST
-    DUPLICATE_EMAIL(BAD_REQUEST, "이미 가입된 이메일입니다");
+    DUPLICATE_EMAIL(BAD_REQUEST, "이미 가입된 이메일입니다"),
+
+    /* 401 UNAUTHORIZED: 인증 실패 */
+    UNAUTHORIZED_USER(UNAUTHORIZED, "만료되었거나 잘못된 토큰입니다. 토큰을 확인해주세요.");
 
     private final HttpStatus status;
     private final String message;
