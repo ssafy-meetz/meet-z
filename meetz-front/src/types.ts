208b4e1d0f7cc579e321c78ad582c4a5ccf0d2a2
monam2 <kangcw0107@gmail.com>
2024-07-22 15:56:49 +0900

    [FE] feat: 회원가입 응답 타입을 전역 타입 으로 관리(#20)

--- meetz-front/src/types.ts
+++ meetz-front/src/types.ts
@@ -1,13 +1,17 @@
 /**
  * 로그인 API 통신 시 사용하는 타입
- * @types {refreshToken}
- * @types {accessToken}
- * @types {expireAt}
- * @types {role}
  */
 export type LoginUserDto = {
     refreshToken: string;
     accessToken: string;
     expireAt: string;
     role: string;
+}
+
+/**
+ * 회원가입 API 통신 시 사용하는 타입
+ */
+export type SignupResDto = {
+    status: string;
+    message: string;
 }
