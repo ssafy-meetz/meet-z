f5203fb1d7a3167e6475ba145052d4cb9dfc4b42
손다인 <dain0826@naver.com>
2024-07-22 15:08:24 +0900

    [BE] feat: jwt토큰 검증로직 추가 (#18)

--- meetz-back/meetz/src/main/java/com/c108/meetz/jwt/JWTFilter.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/jwt/JWTFilter.java
@@ -1,7 +1,9 @@
 package com.c108.meetz.jwt;
 
+import com.c108.meetz.constants.ErrorCode;
 import com.c108.meetz.dto.CustomUserDetails;
 import com.c108.meetz.dto.request.CommonDto;
+import com.c108.meetz.exception.CustomException;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -14,6 +16,8 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import static com.c108.meetz.constants.ErrorCode.UNAUTHORIZED_USER;
 
 public class JWTFilter extends OncePerRequestFilter {
    private final JWTUtil jwtUtil;
@@ -31,44 +35,52 @@ public class JWTFilter extends OncePerRequestFilter {
             return;
         }
         //헤더에서 access키에 담긴 토큰을 꺼냄
-        String accessToken = authorization.split(" ")[1];
+        String token = authorization.split(" ")[1];
 
         //토큰이 없다면 다음 필터로 넘김
-        if(accessToken == null){
+        if(token == null){
             filterChain.doFilter(request, response);
             return;
         }
         //토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
         try {
-            jwtUtil.isExpired(accessToken);
+            jwtUtil.isExpired(token);
         } catch (ExpiredJwtException e) {
             logger.debug("JWTFilter: 토큰 만료");
-            //response body
-            PrintWriter writer = response.getWriter();
-            writer.print("access token expired");
+            throw new CustomException(UNAUTHORIZED_USER);
 
             //response status code
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
+//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//            return;
         }
 
         //토큰이 access인지 확인 (발급시 페이로드에 명시)
-        String category = jwtUtil.getCategory(accessToken);
-
-        if (!category.equals("access")) {
-            logger.debug("JWTFilter: 잘못된 토큰 카테고리");
-            //response body
-            PrintWriter writer = response.getWriter();
-            writer.print("invalid access token");
-
-            //response status code
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
+        String category = jwtUtil.getCategory(token);
+        String requestURI = request.getRequestURI();
+        if(requestURI.equals("/api/refresh")){
+            if(!category.equals("refresh")){
+                throw new CustomException(UNAUTHORIZED_USER);
+            }
         }
+        else{
+            if(!category.equals("access")){
+                throw new CustomException(UNAUTHORIZED_USER);
+            }
+        }
+//        if (!category.equals("access")) {
+//            logger.debug("JWTFilter: 잘못된 토큰 카테고리");
+//            //response body
+//            PrintWriter writer = response.getWriter();
+//            writer.print("invalid access token");
+//
+//            //response status code
+//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//            return;
+//        }
 
         //username, role값을 획득
-        String email = jwtUtil.getEmail(accessToken);
-        String role = jwtUtil.getRole(accessToken);
+        String email = jwtUtil.getEmail(token);
+        String role = jwtUtil.getRole(token);
 
         CommonDto commonDto = new CommonDto();
         commonDto.setEmail(email);
