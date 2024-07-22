ccfa2c4668f7bec3ef5882a87a91a420c2cb1a52
손다인 <dain0826@naver.com>
2024-07-22 15:09:24 +0900

    [BE] feat: ãAuthentication Exception handle로ì 설정 (#18)

---
+++ meetz-back/meetz/src/main/java/com/c108/meetz/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,25 @@
+package com.c108.meetz.exception;
+
+import com.c108.meetz.constants.ErrorCode;
+import com.c108.meetz.dto.ApiResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        ApiResponse<Object> responseBody = ApiResponse.error(ErrorCode.UNAUTHORIZED_USER);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write(new ObjectMapper().writeValueAsString(responseBody));
+    }
+}
---
+++ meetz-back/meetz/src/main/java/com/c108/meetz/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package com.c108.meetz.exception;
+
+import com.c108.meetz.constants.ErrorCode;
+import com.c108.meetz.dto.ApiResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ApiResponse<Object> responseBody = ApiResponse.error(ErrorCode.UNAUTHORIZED_USER);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write(new ObjectMapper().writeValueAsString(responseBody));
+    }
+}
