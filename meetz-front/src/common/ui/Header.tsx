80822b0586ac6da4ceac6754c25d96f641e83b88
Seo Minsu <seominsu@Soo-MacBookPro.local>
2024-07-24 01:23:56 +0900

    [FE]: 헤더 라우터링크 추가 (#41)

--- meetz-front/src/common/ui/Header.tsx
+++ meetz-front/src/common/ui/Header.tsx
@@ -1,16 +1,17 @@
 import logo from '/src/assets/images/logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className=' bg-white border-b border-gray-300 border-opacity-50 flex  justify-center'>
       
       <div className='flex max-w-screen-xl justify-between items-center w-full px-9 py-10'>
-          <button>
+          <Link to='yet'>
             <img src={logo} alt='Meet:Z 로고' className='h-8' />
-          </button>
+          </Link>
         <div className='flex justify-between w-full max-w-sm'>
-          <button className='text-2xl font-bold'>완료된 미팅</button>
-          <button className='text-2xl font-bold'>미완료 미팅</button>
+          <Link to='end' className='text-2xl font-bold'>완료된 미팅</Link>
+          <Link to='yet' className='text-2xl font-bold'>미완료 미팅</Link>
           <button className='text-2xl font-bold'>로그아웃</button>
         </div>
       </div>
