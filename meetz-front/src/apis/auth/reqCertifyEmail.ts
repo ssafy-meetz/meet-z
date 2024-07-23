be644242e8f132fdb67fd7629324b24296993e4b
monam2 <kangcw0107@gmail.com>
2024-07-23 16:19:02 +0900

    [FE] feat: 로그인/회원가입 API 통신 코드 수정(#34)

--- meetz-front/src/apis/auth/reqCertifyEmail.ts
+++ meetz-front/src/apis/auth/reqCertifyEmail.ts
@@ -2,8 +2,13 @@ import instance from "../axios";
 
 const reqCertifyEmail = async (eamil: string) => {
     try {
-        const res = await instance.post(`api/manager/authemail?email=${eamil}`)
-        return res.status === 200;
+        const { data } = await instance.get(`api/manager/authemail?email=${eamil}`)
+
+        if (data.status === 400) {
+            return false;
+        } else {
+            return true;
+        }
     } catch (e) {
         throw new Error(`이메일 인증 요청 실패 : ${e}`)
     }
