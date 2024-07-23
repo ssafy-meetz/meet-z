fff55a5411b415dd62e253de1574e11dd18a66f8
손다인 <dain0826@naver.com>
2024-07-24 01:27:21 +0900

    [BE] fix: valid 예외 추가  (#35)

--- meetz-back/meetz/src/main/java/com/c108/meetz/api/CommonApi.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/api/CommonApi.java
@@ -4,11 +4,14 @@ import com.c108.meetz.dto.ApiResponse;
 import com.c108.meetz.dto.request.CommonDto;
 import com.c108.meetz.dto.request.LoginRequestDto;
 import com.c108.meetz.dto.response.LoginResponseDto;
+import com.c108.meetz.exception.BadRequestException;
 import com.c108.meetz.service.CommonService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import static com.c108.meetz.constants.ErrorCode.FAIL_TO_LOGIN;
 import static com.c108.meetz.constants.SuccessCode.LOGIN_SUCCESS;
 import static com.c108.meetz.constants.SuccessCode.TOKEN_REISSUE_SUCCESS;
 import static com.c108.meetz.constants.SuccessCode.CHECK_INFO_SUCCESS;
@@ -20,7 +23,10 @@ public class CommonApi {
     private final CommonService commonService;
 
     @PostMapping("/login")
-    public ApiResponse<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto loginRequestDto){
+    public ApiResponse<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto loginRequestDto, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new BadRequestException(FAIL_TO_LOGIN);
+        }
         LoginResponseDto response = commonService.login(loginRequestDto);
         return ApiResponse.success(LOGIN_SUCCESS, response);
     }
