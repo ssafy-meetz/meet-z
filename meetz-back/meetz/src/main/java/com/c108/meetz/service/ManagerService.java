4ad16d5120ce69ce811948de55028e45407dca45
손다인 <dain0826@naver.com>
2024-07-23 11:11:50 +0900

    [BE] feat: 매니저 회원가ì회원가입시 이메일 중복 한번 더 체크 (#31)

--- meetz-back/meetz/src/main/java/com/c108/meetz/service/ManagerService.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/service/ManagerService.java
@@ -1,11 +1,15 @@
 package com.c108.meetz.service;
 
+import com.c108.meetz.constants.ErrorCode;
 import com.c108.meetz.domain.Manager;
 import com.c108.meetz.dto.request.JoinRequestDto;
+import com.c108.meetz.exception.CustomException;
 import com.c108.meetz.repository.ManagerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import static com.c108.meetz.constants.ErrorCode.DUPLICATE_EMAIL;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +19,9 @@ public class ManagerService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public void joinManager(JoinRequestDto joinRequestDto){
+        if(managerRepository.findByEmail(joinRequestDto.getEmail()).isPresent()){
+            throw new CustomException(DUPLICATE_EMAIL);
+        }
         Manager manager = joinRequestDto.toManager(bCryptPasswordEncoder);
         managerRepository.save(manager);
     }
