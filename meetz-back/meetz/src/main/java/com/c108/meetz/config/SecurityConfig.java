975a3afdf6befc1b07ca7ba18155badf777b2e77
unknown <wlk256032@gmail.com>
2024-07-24 12:06:14 +0900

    [BE] confing: testApi추가

--- meetz-back/meetz/src/main/java/com/c108/meetz/config/SecurityConfig.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/config/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/login", "/", "/api/manager/join").permitAll()
                         .requestMatchers("/api/refresh").permitAll()
                         .requestMatchers("/api/manager/authemail", "/api/manager/checkemail").permitAll()
-                        .requestMatchers("/api/manager/checkauth").permitAll()
+                        .requestMatchers("/api/manager/checkauth", "/api/manager/test").permitAll()
                         .anyRequest().authenticated());
         // 예외 처리 설정
         http.exceptionHandling(exceptionHandling -> exceptionHandling
