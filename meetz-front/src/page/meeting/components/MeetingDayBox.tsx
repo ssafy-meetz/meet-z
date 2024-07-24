d86e99067592ced2164caee4ad4508fc06a7e13b
monam2 <kangcw0107@gmail.com>
2024-07-24 17:51:04 +0900

    [FE] feat: 미팅 예정/완료 페이지 버튼 라우팅(#46)

---
+++ meetz-front/src/page/meeting/components/MeetingDayBox.tsx
@@ -0,0 +1,14 @@
+const MeetingDayBox = () => {
+  return (
+    <div className='text-center mr-6 '>
+      <div className='text-[36px] font-bold whitespace-nowrap'>
+        16일
+      </div>
+      <div className='text-[32px] font-semibold text-[#7d7d7d]'>
+        Tue
+      </div>
+    </div>
+  )
+}
+
+export default MeetingDayBox
--- meetz-front/src/page/meeting/components/MeetingList.tsx
+++ meetz-front/src/page/meeting/components/MeetingList.tsx
@@ -1,3 +1,4 @@
+import MeetingDayBox from "./MeetingDayBox";
 import MeetingListItem from "./MeetingListItem";
 
 interface MeetingListProps {
@@ -15,15 +16,8 @@ const MeetingList = ({ month }: MeetingListProps) => {
       </div>
       <div className='flex flex-col items-center py-20 bg-[#f9f9f9]'>
         <div className='max-w-screen-xl w-screen gap-12 flex flex-col justify-center'>
-          <div className='flex px-24 '>
-            <div className='text-center mr-6 '>
-              <div className='text-[36px] font-bold whitespace-nowrap'>
-                16일
-              </div>
-              <div className='text-[32px] font-semibold text-[#7d7d7d]'>
-                Tue
-              </div>
-            </div>
+          <div className='flex px-24'>
+            <MeetingDayBox />
             <div className='flex flex-col w-full gap-5'>
               <MeetingListItem />
               <MeetingListItem />
