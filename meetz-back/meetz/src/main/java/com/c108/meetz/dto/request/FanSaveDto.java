0149116a476b0f353a2cf5be5a2fbdbca1b90bcf
taeyeon <kam_ta_yon@naver.com>
2024-07-26 17:39:22 +0900

    [BE]feat: 도메인 팬 저장 Dto 생성(#55)

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/request/FanSaveDto.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/request/FanSaveDto.java
@@ -1,2 +1,13 @@
-package com.c108.meetz.dto.request;public class FanSaveDto {
+package com.c108.meetz.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FanSaveDto {
+
+    private String name;
+    private String email;
+    private String phone;
 }
