de0c0affdb65ac6d85442b32251f7ca1748a6542
taeyeon <kam_ta_yon@naver.com>
2024-07-26 17:37:24 +0900

    [BE] feat: 미팅 도메인 생성

--- meetz-back/meetz/src/main/java/com/c108/meetz/domain/Meeting.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/domain/Meeting.java
@@ -1,11 +1,13 @@
 package com.c108.meetz.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -14,27 +16,29 @@ public class Meeting {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int meeting_id;
+    private int meetingId;
 
     @ManyToOne
-    @JoinColumn(name = "manager_id", nullable = false)
+    @JoinColumn(name = "managerId", nullable = false)
     private Manager manager;
 
     @NotNull
     @Column(nullable = false)
-    private String meeting_name;
+    private String meetingName;
 
     @NotNull
     @Column(nullable = false)
-    private Timestamp meeting_start;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime meetingStart;
 
     @NotNull
     @Column(nullable = false)
-    private int meeting_duration;
+    private int meetingDuration;
 
     @NotNull
-    @Column(nullable = false)
-    private Timestamp meeting_end;
+    @Column(nullable = true)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime meetingEnd;
 
     @NotNull
     @Column(nullable = false)
