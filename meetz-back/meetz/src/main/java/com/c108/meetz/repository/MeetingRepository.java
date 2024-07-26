fadebf2521483093cf94addea6c16f27a2cd5bca
taeyeon <kam_ta_yon@naver.com>
2024-07-26 17:42:55 +0900

    [BE]feat: 미팅 레포지터리 생성(#55)

--- meetz-back/meetz/src/main/java/com/c108/meetz/repository/MeetingRepository.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/repository/MeetingRepository.java
@@ -1,2 +1,8 @@
-package com.c108.meetz.repository;public interface MeetingRepository {
+package com.c108.meetz.repository;
+
+import com.c108.meetz.domain.Meeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MeetingRepository extends JpaRepository<Meeting, Integer> {
+
 }
