67f0bafaaf833998b94596796b10c938398919ff
taeyeon <kam_ta_yon@naver.com>
2024-07-26 17:40:33 +0900

    [BE]feat: 미팅 저장 Request Dto 생성(#55)

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/request/MeetingSaveRequestDto.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/request/MeetingSaveRequestDto.java
@@ -1,45 +1,53 @@
 package com.c108.meetz.dto.request;
 
+import com.c108.meetz.domain.Manager;
+import com.c108.meetz.domain.Meeting;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
-public class MeetingRequestDto {
+public class MeetingSaveRequestDto {
+
+    @NotNull
+    private int managerId;
 
     @NotBlank(message = "미팅 이름은 필수로 입력해야 합니다.")
-    private String meeting_name;
+    private String meetingName;
 
-    @NotBlank(message = "미팅 시작 시간은 필수로 입력해야 합니다.")
-    private Timestamp meeting_start;
+    @NotNull(message = "미팅 시작 시간은 필수로 입력해야 합니다.")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime meetingStart;
 
-    @NotBlank(message = "미팅 시간은 필수로 입력해야 합니다.")
-    private int meeting_duration;
+    @NotNull(message = "미팅 시간은 필수로 입력해야 합니다.")
+    private int meetingDuration;
 
-    @NotBlank
-    private Timestamp meeting_end;
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime meetingEnd;
 
-    @NotBlank(message = "쉬는 시간은 필수로 입력해야 합니다.")
+    @NotNull(message = "쉬는 시간은 필수로 입력해야 합니다.")
     private int term;
 
-    private List<StarDTO> starList;
-    private List<FanDTO> fanList;
+    private List<StarSaveDto> starList;
+    private List<FanSaveDto> fanList;
 
-    @Getter
-    @Setter
-    public static class StarDTO {
-        private String name;
-    }
-
-    @Getter
-    @Setter
-    public static class FanDTO {
-        private String name;
-        private String email;
-        private String phone;
+    /*다인이가 위에 코드 이렇게 바꿈*/
+    public Meeting toMeeting(Manager manager){
+        Meeting meeting = new Meeting();
+        meeting.setManager(manager);
+        meeting.setMeetingName(this.meetingName);
+        meeting.setMeetingStart(this.meetingStart);
+        meeting.setMeetingDuration(this.meetingDuration);
+        meeting.setMeetingEnd(this.meetingEnd);
+        meeting.setTerm(this.term);
+        return meeting;
     }
 }
