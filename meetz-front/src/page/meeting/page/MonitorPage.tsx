ff2ab40c2767306385a509f68da14b1552c5e447
monam2 <kangcw0107@gmail.com>
2024-07-28 02:06:02 +0900

    [FE] feat: store의 경고모달창 상태에 따라 모달창 보여주기(#70)

--- meetz-front/src/page/meeting/page/MonitorPage.tsx
+++ meetz-front/src/page/meeting/page/MonitorPage.tsx
@@ -1,8 +1,11 @@
 import useCheckAuth from '../../../hooks/meeting/useCheckAuth';
 import Accordion from '../components/Accordion';
 import Alert from '/src/assets/images/alert.png';
+import WarningModal from '../components/WarningModal';
+import { useMonitorStore } from '../../../zustand/useMonitorStore';
 
 const MonitoringPage = () => {
+  const { warnModalOpend, openWarnModal, closeWarnModal } = useMonitorStore();
 
   useCheckAuth('MANAGER');
 
@@ -18,41 +21,13 @@ const MonitoringPage = () => {
         <span className='text-2xl font-bold'>총 3 건</span>
         <main className='py-7'>
           <div className='justify-center items-center '>
-            <Accordion title='1번 팬 - 라이언'>
-              <div></div>
-            </Accordion>
-            <Accordion title='2번 팬 - 어피치'>
-              <div></div>
-            </Accordion>
-            <Accordion title='3번 팬 - 무지'>
-              <div></div>
-            </Accordion>
+            <Accordion title='1번 팬 - 라이언' />
+            <Accordion title='2번 팬 - 어피치' />
+            <Accordion title='3번 팬 - 무지' />
           </div>
         </main>
       </div>
-      <div className='w-[360px] h-[180px] flex flex-col items-center justify-center rounded-3xl border border-[#FF4F5D]'>
-        {/* 모달창 */}
-        <div className='flex flex-col gap-4'>
-          <div className='gap-3 flex flex-col items-center justify-center'>
-            <div className='w-11 h-11'>
-              <img src={Alert} alt='alert' />
-            </div>
-            <div>
-              <span className='text-base'>
-                "강창우" 팬을 영구 제명 하겠습니까?
-              </span>
-            </div>
-          </div>
-          <div className='flex justify-center gap-4'>
-            <button className='text-sm text-[#FF4F5D] border px-4 py-1 border-[#FF4F5D] hover:text-white rounded-lg hover:bg-[#FF4F5D]'>
-              진행
-            </button>
-            <button className='text-sm text-[#FF4F5D] border px-4 py-1 border-[#FF4F5D] hover:text-white rounded-lg hover:bg-[#FF4F5D]'>
-              취소
-            </button>
-          </div>
-        </div>
-      </div>
+      {warnModalOpend && <WarningModal />}
       <div className='w-[360px] h-[180px] flex flex-col items-center justify-center rounded-3xl border border-[#FF4F5D]'>
         {/* 모달창 */}
         <div className='flex flex-col gap-4'>
