697dda259670e16771a90881f53d84cf66175d99
손다인 <dain0826@naver.com>
2024-07-27 23:39:22 +0900

    [BE] feat: 임시이메일, 임시비밀번호 발급 서비스 작성 (#55)

---
+++ meetz-back/meetz/src/main/java/com/c108/meetz/service/TemporaryGenerator.java
@@ -0,0 +1,27 @@
+package com.c108.meetz.service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.UUID;
+
+public class TemporaryGenerator {
+
+    private static final SecureRandom random = new SecureRandom();
+    private static final int PASSWORD_LENGTH = 12;
+
+    public static String generateTemporaryPassword() {
+        byte[] bytes = new byte[PASSWORD_LENGTH];
+        random.nextBytes(bytes);
+        String encoded = Base64.getUrlEncoder().withoutPadding().encodeToString(bytes).substring(0, PASSWORD_LENGTH);
+        if (encoded.length() > PASSWORD_LENGTH) {
+            return encoded.substring(0, PASSWORD_LENGTH);
+        } else {
+            return encoded;
+        }
+    }
+
+    public static String generateTemporaryEmail() {
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        return uuid + "@meetz.com";
+    }
+}
