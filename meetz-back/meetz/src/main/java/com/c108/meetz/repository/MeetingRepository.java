8c384b2e0fb8b9746e7056d4a16444ac1cea58e2
taeyeon <kam_ta_yon@naver.com>
2024-07-29 01:14:02 +0900

    [BE] feat: MeetingRepository 완료미팅 목록조회-(#78)

--- meetz-back/meetz/src/main/java/com/c108/meetz/repository/MeetingRepository.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/repository/MeetingRepository.java
@@ -2,7 +2,14 @@ package com.c108.meetz.repository;
 
 import com.c108.meetz.domain.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Integer> {
 
+    @Query("SELECT m FROM Meeting m WHERE m.manager.managerId = :managerId AND m.meetingEnd < :currentTime")
+    List<Meeting> findCompletedMeetingsByManagerId(@Param("managerId") int managerId, @Param("currentTime") LocalDateTime currentTime);
 }
