f9bd0bf340f17cae8fdfb6611cfed17e68b62865
monam2 <kangcw0107@gmail.com>
2024-07-28 20:53:04 +0900

    [FE] feat: 미팅 간격 시간 설정 컴포넌트 생성(#77)

---
+++ meetz-front/src/page/meeting/components/CreateAndModify/SetTimeBreakBox.tsx
@@ -0,0 +1,29 @@
+import Select from 'react-select';
+import useMeetingTimeStore from '../../../../zustand/useMeetingTimeStore';
+import { breakOptions, customStyles } from './TimeOptions';
+
+const SetTimeBreakBox = () => {
+  const { selectedBreak, setSelectedBreak } = useMeetingTimeStore();
+
+  return (
+    <div className='flex gap-14 py-5 border-b items-center'>
+      <div className='w-40'>
+        <span className='text-xl text-[#3a3a3a] font-semibold'>
+          쉬는 시간
+        </span>
+      </div>
+      <div>
+        <Select
+          options={breakOptions}
+          value={selectedBreak}
+          onChange={(option) => setSelectedBreak(option)}
+          styles={customStyles}
+          className='text-xl'
+          placeholder='선택'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default SetTimeBreakBox
