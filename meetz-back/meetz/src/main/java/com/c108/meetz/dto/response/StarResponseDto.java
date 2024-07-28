6794e36f696b9fdaf2db728418a62339f1680636
taeyeon <kam_ta_yon@naver.com>
2024-07-28 18:42:50 +0900

    [BE] feat: MeetingDetailResponseDto 미팅상세조회 (#75)

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/StarResponseDto.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/StarResponseDto.java
@@ -1,2 +1,8 @@
-package com.c108.meetz.dto.response;public record StarResponseDto() {
-}
+package com.c108.meetz.dto.response;
+
+public record StarResponseDto(
+        int meetingRoomId,
+        String name,
+        String email,
+        String password
+) {}
