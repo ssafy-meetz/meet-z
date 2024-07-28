4783f38631d398fb91da8525811a4c37a27b8677
monam2 <kangcw0107@gmail.com>
2024-07-28 20:51:16 +0900

    [FE] feat: 미팅 시간 타이틀 컴포넌트 생성(#77)

---
+++ meetz-front/src/page/meeting/components/CreateAndModify/SetTimeHeader.tsx
@@ -0,0 +1,12 @@
+const SetTimeHeader = () => {
+  return (
+    <div className='flex flex-col gap-2 pb-4'>
+      <span className='font-semibold text-2xl'>팬 싸인회 시간 설정</span>
+      <span className='font-light text-base'>
+        팬 싸인회 주최에 필요한 시간 정보를 입력합니다.
+      </span>
+    </div>
+  )
+}
+
+export default SetTimeHeader
