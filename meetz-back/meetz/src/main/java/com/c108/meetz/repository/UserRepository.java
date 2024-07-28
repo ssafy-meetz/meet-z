5a729bca6e89ff305e1c5eda158a35b45f6207c0
손다인 <dain0826@naver.com>
2024-07-28 17:55:21 +0900

    [BE] feat: 미팅id로 fanList 찾는 메서드 추가 (#76)

--- meetz-back/meetz/src/main/java/com/c108/meetz/repository/UserRepository.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.c108.meetz.repository;
 
+import com.c108.meetz.domain.Role;
 import com.c108.meetz.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -14,4 +16,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 
     Boolean existsByToken(String token);
+
+    List<User> findByMeeting_MeetingIdAndRole(int meetingId, Role role);
 }
