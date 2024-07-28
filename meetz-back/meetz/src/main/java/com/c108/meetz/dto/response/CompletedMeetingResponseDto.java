3943b797b6a5cba0bd6bbe169031ac66d372cba7
taeyeon <kam_ta_yon@naver.com>
2024-07-29 01:13:35 +0900

    [BE] feat: CompletedMeetingResponseDto 완료미팅 목록조회-(#78)

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/CompletedMeetingResponseDto.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/CompletedMeetingResponseDto.java
@@ -4,9 +4,10 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
-public record CompletedMeetingDto(
+public record CompletedMeetingResponseDto(
         int meetingId,
         String meetingName,
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime meetingStart,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime meetingEnd,
         int cnt
 ) { }
