e6bd10ca076e90b580267ac5eb38f2e0225b6c7e
서민수 <minsu9812@naver.com>
2024-07-29 15:18:57 +0900

    [FE] feat:헤더 보더 그림자 추가

--- meetz-front/src/common/ui/Header.tsx
+++ meetz-front/src/common/ui/Header.tsx
@@ -10,24 +10,29 @@ const Header = () => {
   const logoutHandler = () => {
     localStorage.clear();
     clearUserData();
-    alert("로그아웃 되었습니다.");
+    alert('로그아웃 되었습니다.');
     navigate('/');
-  }
+  };
 
   return (
-    <div className=' bg-white border-b border-gray-300 border-opacity-50 flex  justify-center'>
+    <div className=' bg-white border-b border-gray-300 border-opacity-50 flex shadow-lg justify-center'>
       <div className='flex max-w-screen-xl justify-between items-center w-full px-9 py-10'>
         <Link to='yet'>
           <img src={logo} alt='Meet:Z 로고' className='h-8' />
         </Link>
         <div className='flex justify-between w-full max-w-sm'>
-          <Link to='end' className='text-2xl font-bold'>완료된 미팅</Link>
-          <Link to='yet' className='text-2xl font-bold'>미완료 미팅</Link>
-          <button className='text-2xl font-bold' onClick={logoutHandler}>로그아웃</button>
+          <Link to='end' className='text-2xl font-bold'>
+            완료된 미팅
+          </Link>
+          <Link to='yet' className='text-2xl font-bold'>
+            미완료 미팅
+          </Link>
+          <button className='text-2xl font-bold' onClick={logoutHandler}>
+            로그아웃
+          </button>
         </div>
       </div>
     </div>
-
   );
 };
 
