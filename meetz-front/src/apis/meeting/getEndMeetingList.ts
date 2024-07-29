396474567c0bcf776d11b58208a2d39a6db474f8
monam2 <kangcw0107@gmail.com>
2024-07-29 21:41:52 +0900

    [FE] feat: 완료/예정 미팅 목록 조회 API 통신 함수 작성(#98)

---
+++ meetz-front/src/apis/meeting/getEndMeetingList.ts
@@ -0,0 +1,22 @@
+import instance from "../axios"
+
+const getEndMeetingList = async (accessToken: string) => {
+  try {
+    const { data } = await instance.get('/api/meeting/completed', {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    })
+
+    if (data.status === 200 && data) {
+      console.log(data)
+    }
+  } catch (error: any) {
+    if (error.response.status === 401) {
+      throw new Error('만료되었거나 잘못된 토큰입니다. 토큰을 확인해주세요.');
+    }
+    throw new Error('알 수 없는 에러가 발생했습니다.');
+  }
+}
+
+export default getEndMeetingList;
