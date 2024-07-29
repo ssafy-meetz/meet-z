017c50edf6df7d6bba7b60e3a990993aa928dab9
monam2 <kangcw0107@gmail.com>
2024-07-29 12:10:50 +0900

    [FE] feat: 팬 관리 모달 상태를 zustand 미팅 관련 store에 생성(#86)

--- meetz-front/src/zustand/useMeetingSettingStore.ts
+++ meetz-front/src/zustand/useMeetingSettingStore.ts
@@ -6,11 +6,13 @@ interface MeetingSeetingState {
     newStar: string;
     inputValue: string;
     inputWidth: number;
+    isOpenModal: boolean;
     setMeetingName: (meetingName: string) => void;
     setStars: (stars: string[]) => void;
     setNewStar: (newStar: string) => void;
     setInputValue: (inputValue: string) => void;
     setInputWidth: (inputWidth: number) => void;
+    setIsOpenModal: () => void;
 }
 
 const useMeetingSettingStore = create<MeetingSeetingState>((set) => ({
@@ -19,11 +21,13 @@ const useMeetingSettingStore = create<MeetingSeetingState>((set) => ({
     newStar: '',
     inputValue: '',
     inputWidth: 70,
+    isOpenModal: false,
     setMeetingName: (meetingName) => set({ meetingName }),
     setStars: (stars) => set({ stars }),
     setNewStar: (newStar) => set({ newStar }),
     setInputValue: (inputValue) => set({ inputValue }),
     setInputWidth: (inputWidth) => set({ inputWidth }),
+    setIsOpenModal: () => set((state) => ({ isOpenModal: !state.isOpenModal }))
 }));
 
 export default useMeetingSettingStore;
