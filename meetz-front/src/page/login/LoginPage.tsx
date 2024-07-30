5afae0918b308e9692b495d903914398507d9373
서민수 <minsu9812@naver.com>
2024-07-30 11:08:57 +0900

    [FE] feat: login,signup UI 크기 조절 (#93)

--- meetz-front/src/page/login/LoginPage.tsx
+++ meetz-front/src/page/login/LoginPage.tsx
@@ -3,14 +3,17 @@ import loginImage from '/src/assets/images/login_image.png';
 
 const LoginPage = () => {
   return (
-    <div className=' bg-gradient-to-br from-[#FE9374] to-[#FE4D5C] w-full h-screen flex flex-col justify-center '>
-      <div className='flex justify-center '>
-        <div className='flex justify-between max-w-screen-xl items-center px-28 gap-20'>
-          <div className='w-full'>
-          <img src={loginImage} alt='로그인 로고'  />
-          </div>
-          <div className='mb-[100px]'>
+    <div className='bg-gradient-to-br from-[#FE9374] to-[#FE4D5C] w-full h-screen flex flex-col justify-center items-center'>
+      <div className='flex flex-col lg:flex-row justify-center items-center w-full max-w-screen-xl px-5 lg:px-28 gap-5 lg:gap-20'>
+        <div className='w-full  justify-center lg:justify-end lg:flex hidden'>
+          <img src={loginImage} alt='로그인 로고' className='w-3/4 lg:w-full' />
+        </div>
+        <div className='w-full flex justify-center lg:justify-start'>
+          <div className='lg:mb-[100px]'>
             <LoginBox />
+            <p className='text-white text-center text-xl mt-8 lg:hidden text-shadow-glow'>
+              해당 서비스는 PC에서만 이용 가능합니다.
+            </p>
           </div>
         </div>
       </div>
