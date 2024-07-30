71dbbb5b40674aacbfa17902b73a3056bdaf82e5
손다인 <dain0826@naver.com>
2024-07-30 19:53:02 +0900

    [BE] develop 브랜치와 다시 동일하게 수정

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/FanListResponseDto.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/FanListResponseDto.java
@@ -1,39 +1,7 @@
 package com.c108.meetz.dto.response;
 
-import com.c108.meetz.domain.User;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.List;
-@Getter
-public class FanListResponseDto {
-
-    private List<FanList> fanList;
-    private FanListResponseDto(List<FanList> fanList) {
-        this.fanList = fanList;
-    }
-
-    public static FanListResponseDto of(List<FanList> fanList){
-        return new FanListResponseDto(fanList);
-    }
-
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    public static class FanList{
-        private int userId;
-        private String name;
-        private String email;
-        private String phone;
-
-        public static FanList of(User user) {
-            FanList fanList = new FanList();
-            fanList.userId = user.getUserId();
-            fanList.name = user.getName();
-            fanList.email = user.getEmail();
-            fanList.phone = user.getPhone();
-            return fanList;
-        }
-    }
-}
+public record FanListResponseDto(
+        int meetingId,
+        List<FanResponseDto> fanList
+) { }
