dc3d4f40ba13711ba1182836f91c49d96333a390
손다인 <dain0826@naver.com>
2024-07-30 17:48:31 +0900

    [BE] refactor: 스타 목록 조회 Dto 통합 (#109)

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/StarListResponseDto.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/StarListResponseDto.java
@@ -1,8 +1,39 @@
 package com.c108.meetz.dto.response;
 
+import com.c108.meetz.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
-public record StarListResponseDto(
-        int meetingId,
-        List<StarResponseDto> starList
-) { }
+@Getter
+public class StarListResponseDto {
+    private List<StarList> starList;
+    private StarListResponseDto(List<StarList> starList){
+        this.starList = starList;
+    }
+
+    public static StarListResponseDto of(List<StarList> starList){
+        return new StarListResponseDto(starList);
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class StarList{
+        private int meetingRoomId;
+        private String name;
+        private String email;
+        private String password;
+
+        public static StarList of(User user){
+            StarList starList = new StarList();
+            starList.name = user.getName();
+            starList.email = user.getEmail();
+            starList.password = user.getPassword();
+            return starList;
+        }
+
+    }
+}
