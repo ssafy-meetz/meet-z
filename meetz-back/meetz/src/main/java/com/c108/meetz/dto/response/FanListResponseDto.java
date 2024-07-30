c17fc006996ba99aa5758f8da22e2099db9dcca8
손다인 <dain0826@naver.com>
2024-07-30 17:48:06 +0900

    [BE] refactor: 팬ë¨ 목록 조회 DTO 통합 (#109)

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/FanListResponseDto.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/FanListResponseDto.java
@@ -1,8 +1,39 @@
 package com.c108.meetz.dto.response;
 
-import java.util.List;
+import com.c108.meetz.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record FanListResponseDto(
-        int meetingId,
-        List<FanResponseDto> fanList
-) { }
+import java.util.List;
+@Getter
+public class FanListResponseDto {
+
+    private List<FanList> fanList;
+    private FanListResponseDto(List<FanList> fanList) {
+        this.fanList = fanList;
+    }
+
+    public static FanListResponseDto of(List<FanList> fanList){
+        return new FanListResponseDto(fanList);
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class FanList{
+        private int userId;
+        private String name;
+        private String email;
+        private String phone;
+
+        public static FanList of(User user) {
+            FanList fanList = new FanList();
+            fanList.userId = user.getUserId();
+            fanList.name = user.getName();
+            fanList.email = user.getEmail();
+            fanList.phone = user.getPhone();
+            return fanList;
+        }
+    }
+}
