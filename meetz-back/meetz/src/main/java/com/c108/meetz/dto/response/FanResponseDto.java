331374107c2dcd22845f1a643a097efcf06cb2bb
손다인 <dain0826@naver.com>
2024-07-31 04:06:54 +0900

    [BE] refactor: user를 주입받는 정적팩토리메서드 생성(#116)

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/FanResponseDto.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/FanResponseDto.java
@@ -1,9 +1,15 @@
 package com.c108.meetz.dto.response;
 
+import com.c108.meetz.domain.User;
+
 public record FanResponseDto(
         int userId,
         String name,
         String email,
         String phone
-) {}
+) {
 
+    public static FanResponseDto from(User user) {
+        return new FanResponseDto(user.getUserId(), user.getName(), user.getEmail(), user.getPhone());
+    }
+}
