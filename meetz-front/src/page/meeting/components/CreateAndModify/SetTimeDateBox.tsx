5cb78daa0e2d420dc659cf658c9a3917f588afa1
monam2 <kangcw0107@gmail.com>
2024-07-31 12:13:16 +0900

    [FE] refactor: DatePicker를 Date input으로 변경(#115)

--- meetz-front/src/page/meeting/components/CreateAndModify/SetTimeDateBox.tsx
+++ meetz-front/src/page/meeting/components/CreateAndModify/SetTimeDateBox.tsx
@@ -1,8 +1,17 @@
-import DatePicker from "react-datepicker"
+import { addDays } from 'date-fns';
 import useMeetingTimeStore from "../../../../zustand/useMeetingTimeStore";
 
 const SetTimeDateBox = () => {
   const { selectedDate, setSelectedDate } = useMeetingTimeStore();
+
+  const handleDateChange = (date: Date | null) => {
+    if (date) {
+      const updatedDate = addDays(date, 1);
+      setSelectedDate(updatedDate);
+    } else {
+      setSelectedDate(null);
+    }
+  };
 
   return (
     <div className='flex gap-14 py-5 border-b items-center'>
@@ -10,16 +19,10 @@ const SetTimeDateBox = () => {
         <span className='text-xl text-[#3a3a3a] font-semibold'>날짜</span>
       </div>
       <div>
-        <DatePicker
-          selected={selectedDate}
-          onChange={(date) => setSelectedDate(date)}
-          dateFormat='yyyy-MM-dd'
-          className='text-xl focus:outline-none cursor-pointer'
-          placeholderText='날짜 선택 '
-        />
+        <input type="date" onChange={(e) => setSelectedDate(new Date(e.target.value))} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SetTimeDateBox
+export default SetTimeDateBox;
