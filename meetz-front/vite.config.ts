c2d2249cff36cdbd6c467989edb03a33c9160476
신민경 <tlsalsrud252@naver.com>
2024-07-31 10:47:10 +0900

    [FE]config: web socket 에러를 해결하기 위한 config 수정 (#88)

--- meetz-front/vite.config.ts
+++ meetz-front/vite.config.ts
@@ -1,18 +1,34 @@
-import { defineConfig,loadEnv } from 'vite';
+import { defineConfig,loadEnv,UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+// Load environment variables based on the mode
+const env = loadEnv(mode, process.cwd());
 
-  server:{
-    host: '0.0.0.0',
-    port:3000,
-    proxy:{
-      '/api':{
-        target: 'https://i11c108.p.ssafy.io/',
-        changeOrigin:true,
-      }
-    }
-  },
-  plugins: [react()],
+// Common configuration
+const config = {
+plugins: [react()],
+server: {
+host: '0.0.0.0',
+port: 3000,
+proxy: {
+'/api': {
+target: env.VITE_API_LOCAL_URL || 'http://localhost:8080',
+changeOrigin: true,
+},
+},
+},
+};
+
+// Add HMR configuration for production
+if (env.VITE_MODE != 'local') {
+(config.server as any).hmr = {
+host: env.VITE_PUBLIC_URL.replace('https://', ''),
+port: 443,
+protocol: 'wss',
+};
+}
+
+return config;
 });
