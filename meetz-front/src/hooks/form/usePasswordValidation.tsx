8819ba082bdc5a22b3972362b40139e4c1b15e28
monam2 <kangcw0107@gmail.com>
2024-08-01 11:24:28 +0900

    [FE] fix: 비밀번호 유효성 검사 커스텀 훅 에러 수정(#126)

--- meetz-front/src/hooks/form/usePasswordValidation.tsx
+++ meetz-front/src/hooks/form/usePasswordValidation.tsx
@@ -15,15 +15,13 @@ const usePasswordValidation = () => {
 
     const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newPassword = e.target.value;
-        console.log('Password:', newPassword); // 비밀번호 입력값을 확인합니다.
         setPassword(newPassword);
         const valid = validatePassword(newPassword);
-        console.log('Is Valid:', valid); // 유효성 검사 결과를 확인합니다.
         setIsValidPassword(valid);
     }
 
     return {
-        password, isValidPassword, handlePasswordChange
+        password, isValidPassword, setPassword, handlePasswordChange
     }
 }
 
