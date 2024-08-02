cc4f5856b1c5af84f96021caf3be5981f0d35824
monam2 <kangcw0107@gmail.com>
2024-08-02 10:29:18 +0900

    [FE] style: 불필요 코드 제거

--- meetz-front/src/page/meeting/MeetingLayout.tsx
+++ meetz-front/src/page/meeting/MeetingLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from '../../common/ui/Header';
 import Footer from '../../common/ui/Footer';
 import useCheckAuth from '../../hooks/meeting/useCheckAuth';
