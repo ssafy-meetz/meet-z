5ae01cb2ffb6b36713591a721f690a4a27358009
손다인 <dain0826@naver.com>
2024-08-02 17:58:31 +0900

    [BE] feat:: 웹소케켓 JWT 필터 추가 (#122)

--- meetz-back/meetz/src/main/java/com/c108/meetz/config/WebSocketConfig.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.c108.meetz.config;
 
+import com.c108.meetz.jwt.JwtChannelInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final WebSocketHandShakeInterceptor handshakeInterceptor;
+    private final JwtChannelInterceptor jwtChannelInterceptor;
+
+    public WebSocketConfig(WebSocketHandShakeInterceptor handshakeInterceptor, JwtChannelInterceptor jwtChannelInterceptor) {
+        this.handshakeInterceptor = handshakeInterceptor;
+        this.jwtChannelInterceptor = jwtChannelInterceptor;
+    }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub"); //메세지 받을 때 경로
@@ -18,8 +26,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins("https://i11c108.p.ssafy.io", "http://localhost:3000", "http://localhost:5173", "http://i11c108.p.ssafy.io:3000", "http://i11c108.p.ssafy.io:5173")
-                .withSockJS();
+                .addInterceptors(handshakeInterceptor); // 핸드셰이크 인터셉터 추가
     }
-
+    @Override
+    public void configureClientInboundChannel(org.springframework.messaging.simp.config.ChannelRegistration registration) {
+        registration.interceptors(jwtChannelInterceptor); // STOMP 메시지 인터셉터 추가
+    }
 
 }
