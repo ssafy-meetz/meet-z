e35576536c55989c6d6b59c6ad1e4e0197321b39
손다인 <dain0826@naver.com>
2024-08-02 18:01:30 +0900

    [BE] feat: 채팅 리포지토ã리 ì ì 작성 (#122)

--- meetz-back/meetz/src/main/java/com/c108/meetz/repository/ChatRepository.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/repository/ChatRepository.java
@@ -2,6 +2,15 @@ package com.c108.meetz.repository;
 
 import com.c108.meetz.domain.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Integer> {
+
+    @Query("select c from Chat c where c.chatRoom.chatRoomId =: chatRoomId order by c.createdAt desc limit 1")
+    Chat findFirstChatByChatRoomId(int chatRoomId);
+
+    @Query("select c from Chat c where c.chatRoom.chatRoomId =: chatRoomId order by c.createdAt")
+    List<Chat> findAllChatByChatRoomId(int chatRoomId);
 }
