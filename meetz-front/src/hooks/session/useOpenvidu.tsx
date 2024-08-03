3bdebe0f06741d6ddc465e297c197a8ac3840f69
minggwen <tlsalsrud252@naver.com>
2024-08-03 22:15:43 +0900

    [FE] fix: session page 충돌 해결 (#143)

--- meetz-front/src/hooks/session/useOpenvidu.tsx
+++ meetz-front/src/hooks/session/useOpenvidu.tsx
@@ -3,154 +3,103 @@ import { OpenVidu, Session as OVSession, Subscriber, Publisher } from 'openvidu-
 import { createSession, createToken } from '../../apis/session/openviduAPI';
 
 export const useOpenvidu = () => {
-  const [session, setSession] = useState<OVSession | ''>('');
-  const [sessionId, setSessionId] = useState<string>('');
-  const [subscriber, setSubscriber] = useState<Subscriber | null>(null);
-  const [publisher, setPublisher] = useState<Publisher | null>(null);
-  const [OV, setOV] = useState<OpenVidu | null>(null);
+    const [session, setSession] = useState<OVSession | ''>('');
+	const [sessionId, setSessionId] = useState<string>('');
+	const [subscriber, setSubscriber] = useState<Subscriber | null>(null);
+	const [publisher, setPublisher] = useState<Publisher | null>(null);
+	const [OV, setOV] = useState<OpenVidu | null>(null);
 
   const leaveSession = useCallback(() => {
-    if (session) session.disconnect();
+        if (session) session.disconnect();
 
-    setOV(null);
-    setSession('');
-    setSessionId('');
-    setSubscriber(null);
-    setPublisher(null);
-  }, [session]);
-
-  const joinSession = () => {
-
-    console.log(sessionId)
-    if (sessionId != '') return;
-    const OVs = new OpenVidu();
-    sessionIdChangeHandler();
-    setOV(OVs);
-    setSession(OVs.initSession());
-  };
-
-  useEffect(() => {
-    window.addEventListener('beforeunload', leaveSession);
-
-    return () => {
-      window.removeEventListener('beforeunload', leaveSession);
-    };
-  }, [leaveSession]);
-
-  const sessionIdChangeHandler = (
-    // event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setSessionId('meetz');
-  };
-
-  useEffect(() => {
-    if (session === '') return;
-
-    session.on('streamDestroyed', event => {
-      if (subscriber && event.stream.streamId === subscriber.stream.streamId) {
+        setOV(null);
+        setSession('');
+        setSessionId('');
         setSubscriber(null);
-      }
-    });
-  }, [subscriber, session]);
+        setPublisher(null);
+    }, [session]);
 
-  useEffect(() => {
-    if (session === '') return;
-
-    session.on('streamCreated', event => {
-      const subscribers = session.subscribe(event.stream, '');
-      console.log("!!");
-      console.log(subscribers)
-      setSubscriber(subscribers);
-    });
-
-    const getToken = async (): Promise<string> => {
-      try {
-        const sessionIds = await createSession(sessionId);
-        const token = await createToken(sessionIds);
-        return token;
-      } catch (error) {
-        throw new Error('Failed to get token.');
-      }
+    const joinSession = (id:string) => {
+        if(sessionId!='')return;
+        const OVs = new OpenVidu();
+        console.log("!!");
+        console.log(id);
+        setSessionId(id);
+        setOV(OVs);
+        setSession(OVs.initSession());
     };
 
-    getToken()
-      .then(token => {
-        session
-          .connect(token)
-          .then(() => {
-            if (OV) {
-              const publishers = OV.initPublisher(undefined, {
-                audioSource: undefined,
-                videoSource: undefined,
-                publishAudio: true,
-                publishVideo: true,
-                mirror: false,
-              });
+    useEffect(() => {
+        window.addEventListener('beforeunload', leaveSession);
 
-              setPublisher(publishers);
-              session
-                .publish(publishers)
-                .then(() => { })
-                .catch(() => { });
+        return () => {
+            window.removeEventListener('beforeunload', leaveSession);
+        };
+    }, [leaveSession]);
+
+    useEffect(() => {
+        if (session === '') return;
+
+        session.on('streamDestroyed', event => {
+            if (subscriber && event.stream.streamId === subscriber.stream.streamId) {
+                setSubscriber(null);
             }
-          });
-      })
-  }, [subscriber, session]);
+        });
+    }, [subscriber, session]);
 
-  useEffect(() => {
-    if (session === '') return;
+    useEffect(() => {
+		if (session === '') return;
 
-    session.on('streamCreated', event => {
-      const subscribers = session.subscribe(event.stream, '');
-      console.log("!!");
-      console.log(subscribers)
-      setSubscriber(subscribers);
-    });
+		session.on('streamCreated', event => {
+			const subscribers = session.subscribe(event.stream, '');
+			setSubscriber(subscribers);
+		});
 
-    const getToken = async (): Promise<string> => {
-      try {
-        const sessionIds = await createSession(sessionId);
-        const token = await createToken(sessionIds);
-        return token;
-      } catch (error) {
-        throw new Error('Failed to get token.');
-      }
-    };
+		const getToken = async (): Promise<string> => {
+            // 지금은 openvidu한테 바로 만들어달라고 하지만 여기서 백엔드랑 통신할거임
+			try {
+				const sessionIds = await createSession(sessionId);
+				const token = await createToken(sessionIds);
+				return token;
+			} catch (error) {
+				throw new Error('Failed to get token.');
+			}
+		};
 
-    getToken()
-      .then(token => {
-        session
-          .connect(token)
-          .then(() => {
-            if (OV) {
-              const publishers = OV.initPublisher(undefined, {
-                audioSource: undefined,
-                videoSource: undefined,
-                publishAudio: true,
-                publishVideo: true,
-                mirror: true,
+		getToken()
+			.then(token => {
+				session
+					.connect(token)
+					.then(() => {
+						if (OV) {
+							const publishers = OV.initPublisher(undefined, {
+								audioSource: undefined,
+								videoSource: undefined,
+								publishAudio: true,
+								publishVideo: true,
+								mirror: true,
 
-              });
+							});
 
-              setPublisher(publishers);
-              session
-                .publish(publishers)
-                .then(() => { })
-                .catch(() => { });
-            }
-          })
-          .catch(() => { });
-      })
-      .catch(() => { });
-  }, [session, OV, sessionId]);
-  return {
-    session,
-    sessionId,
-    publisher,
-    subscriber,
-    joinSession,
-    setSessionId,
-    leaveSession
-  };
+							setPublisher(publishers);
+							session
+								.publish(publishers)
+								.then(() => {})
+								.catch(() => {});
+						}
+					})
+					.catch(() => {});
+			})
+			.catch(() => {});
+	}, [session, OV, sessionId]);
+    return {
+        session,
+        sessionId,
+        publisher,
+        subscriber,
+        joinSession,
+        setSessionId,
+        leaveSession
+      };
 
 }
--- meetz-front/src/zustand/useSessionStore.ts
+++ meetz-front/src/zustand/useSessionStore.ts
@@ -1,14 +1,10 @@
 import { create } from 'zustand'
 
-interface sessionStore {
-  myNickname: String;
-  yourNickname: String;
-  setMyNickname: (nickname: String) => void,
-  setYourNickname: (nickname: String) => void
+interface sessionStore{
+    starName:String;
+    setStartName:(nickname:String)=>void
 }
-export const useSessionStore = create<sessionStore>((set) => ({
-  myNickname: "",
-  yourNickname: "",
-  setMyNickname: (nickname) => set({ myNickname: nickname }),
-  setYourNickname: (nickname) => set({ yourNickname: nickname })
+export const useSessionStore = create<sessionStore>((set)=>({
+    starName:"",
+    setStartName:(nickname)=>set({starName:nickname}),
 }))
