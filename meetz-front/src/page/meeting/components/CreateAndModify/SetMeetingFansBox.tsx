8d3c737d66a346de4fdda6a357bebd635bd9a936
monam2 <kangcw0107@gmail.com>
2024-08-04 05:25:10 +0900

    [FE] feat: 팬 목록 양식 엑셀 파일 다운로드 기능 추가(#145)

--- meetz-front/src/page/meeting/components/CreateAndModify/SetMeetingFansBox.tsx
+++ meetz-front/src/page/meeting/components/CreateAndModify/SetMeetingFansBox.tsx
@@ -3,6 +3,15 @@ import useMeetingSettingStore from "../../../../zustand/useMeetingSettingStore";
 const SetMeetingFansBox = () => {
   const { setIsOpenModal } = useMeetingSettingStore();
 
+  const downloadFile = () => {
+    const link = document.createElement('a');
+    link.href = `src/assets/fan_list.xlsx`;
+    link.download = 'fan_list.xlsx';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
   return (
     <div className='flex gap-14 py-5 border-b items-center'>
       <div className='w-40'>
@@ -10,11 +19,16 @@ const SetMeetingFansBox = () => {
           당첨 팬 목록 관리
         </span>
       </div>
-      <div>
+      <div className="flex gap-2">
         <button
           onClick={setIsOpenModal}
           className='bg-[#ff4f5d] text-white rounded-xl px-3 py-1'>
           팬 목록 관리
+        </button>
+        <button
+          onClick={downloadFile}
+          className='border-[#ff4f5d] border border-[] text-[#ff4f5d] rounded-xl px-3 py-1'>
+          양식 다운로드
         </button>
       </div>
     </div>
