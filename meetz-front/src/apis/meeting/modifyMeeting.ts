18ddfa2508ce3c43eca0285931dd271e701bf3b0
monam2 <kangcw0107@gmail.com>
2024-08-03 21:59:08 +0900

    [FE] feat: feat: 미팅 수정 API 통신 함수 구현(#141)

--- meetz-front/src/apis/meeting/modifyMeeting.ts
+++ meetz-front/src/apis/meeting/modifyMeeting.ts
@@ -23,8 +23,26 @@ const putMeetingToModify = async ({ meetingName, meetingStart, meetingDuration, 
         'Authorization': `Bearer ${accessToken}`
       }
     })
-  } catch (error) {
 
+    if (data.code === 200) {
+      return data;
+    }
+  } catch (error: any) {
+    if (error.response) {
+      const status = error.response.status;
+      switch (status) {
+        case 401:
+          throw new Error("접근 권한이 없습니다.");
+        case 404:
+          throw new Error("Meeting not found");
+        case 400:
+          throw new Error("접근 권한이 없습니다.");
+        default:
+          throw new Error("알 수 없는 오류가 발생했습니다.");
+      }
+    } else {
+      throw new Error("네트워크 오류 또는 서버가 응답하지 않습니다.");
+    }
   }
 }
 
