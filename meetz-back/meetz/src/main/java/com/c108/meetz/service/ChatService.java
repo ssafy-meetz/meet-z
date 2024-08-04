133663fd0c320225c716d4b0a0324f22b927c81c
손다인 <dain0826@naver.com>
2024-08-05 00:10:48 +0900

    [BE] feat: chatRoom 최근 메시지 전송시각 업데이트 코드 제거 (#122)

--- meetz-back/meetz/src/main/java/com/c108/meetz/service/ChatService.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/service/ChatService.java
@@ -2,11 +2,7 @@ package com.c108.meetz.service;
 
 import com.c108.meetz.domain.Chat;
 import com.c108.meetz.domain.ChatRoom;
-import com.c108.meetz.domain.Manager;
-import com.c108.meetz.domain.User;
 import com.c108.meetz.dto.request.ChatDto;
-import com.c108.meetz.dto.response.ChatListResponseDto;
-import com.c108.meetz.exception.BadRequestException;
 import com.c108.meetz.exception.NotFoundException;
 import com.c108.meetz.repository.ChatRepository;
 import com.c108.meetz.repository.ChatRoomRepository;
@@ -32,14 +28,17 @@ public class ChatService {
         int id = getId(role, email);
         ChatRoom chatRoom = chatRoomRepository.findById(chatDto.getChatRoomId()).orElseThrow(()->
                 new NotFoundException("chatroom not found"));
+        if(chatDto.getReceiverId() == 0){
+            chatDto.setReceiverId(chatRoom.getMeeting().getManager().getManagerId());
+        }
         Chat chat = Chat.builder()
                 .chatRoom(chatRoom)
-                .role(role)
+                .senderRole(role)
                 .senderId(id)
+                .receiverId(chatDto.getReceiverId())
                 .content(chatDto.getContent())
                 .build();
         chatRepository.save(chat);
-        chatRoom.setRecentChat(LocalDateTime.now());
     }
 
     private int getId(String role, String email){
@@ -50,6 +49,5 @@ public class ChatService {
             return managerRepository.findByEmail(email).orElseThrow(()-> new NotFoundException("manager not found")).getManagerId();
         }
     }
-
 
 }
