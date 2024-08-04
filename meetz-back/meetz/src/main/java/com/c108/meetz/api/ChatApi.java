669bed3705a88dcab30ce0635f629c8e1d6df998
손다인 <dain0826@naver.com>
2024-08-04 23:05:27 +0900

    [BE] feat: 메시지 전송 api (#122)

--- meetz-back/meetz/src/main/java/com/c108/meetz/api/ChatApi.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/api/ChatApi.java
@@ -1,12 +1,19 @@
 package com.c108.meetz.api;
 
+import com.c108.meetz.dto.ApiResponse;
 import com.c108.meetz.dto.request.ChatDto;
+import com.c108.meetz.dto.response.ChatListResponseDto;
+import com.c108.meetz.service.ChatRoomService;
 import com.c108.meetz.service.ChatService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,4 +27,5 @@ public class ChatApi {
         sendingOperations.convertAndSend("/sub/chatRoom/" + chat.getChatRoomId(), chat);
         chatService.saveChat(chat);
     }
+
 }
