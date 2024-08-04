7e36c30c34badc79d9db69dd959f3e6ca980a804
이승원 <wlk256032@naver.com>
2024-08-04 16:39:46 +0900

    [BE] config: SseApi 비활성화

--- meetz-back/meetz/src/main/java/com/c108/meetz/api/SseApi.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/api/SseApi.java
@@ -19,43 +19,43 @@ public class SseApi {
 
     private final SseService sseService;
 
-    //sse연결 시작
-    @GetMapping(path = "/connect/{email}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@PathVariable("email") String email){
-        SseEmitter emitter = sseService.subscrib(email);
-        log.info("Subscribed to {}", emitter);
-        return emitter;
-    }
-
-    //모든 클라이언트들에게 메세지 전달
-    @GetMapping(path = "/broadcast")
-    public ApiResponse<String> boardcastTest(@RequestParam("message") String message){
-        sseService.broadcast(message);
-        log.info("Broadcasted completed");
-        return ApiResponse.success(HttpStatus.OK, "Broadcast completed");
-    }
-
-    //특정 클라이언트에게 메세지 전달
-    @GetMapping(path = "/send")
-    public ApiResponse<String> sendEvent(@RequestParam("email") String email, @RequestParam("message") String message){
-
-        sseService.sendEvent(email, message);
-
-        return ApiResponse.success(HttpStatus.OK, "Send completed");
-    }
-
-    //모든 클라이언트의 이메일 확인(확인용)
-    @GetMapping(path = "/getEmitters")
-    public ApiResponse<String> getAllClientId(){
-
-        List<String> allClientEmail = sseService.getAllClientEmail();
-
-        StringBuilder sb = new StringBuilder();
-
-        for (String email : allClientEmail) {
-            sb.append(email).append("\n");
-        }
-        return ApiResponse.success(HttpStatus.OK, sb.toString());
-    }
+//    //sse연결 시작
+//    @GetMapping(path = "/connect/{email}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public SseEmitter subscribe(@PathVariable("email") String email){
+//        SseEmitter emitter = sseService.subscrib(email);
+//        log.info("Subscribed to {}", emitter);
+//        return emitter;
+//    }
+//
+//    //모든 클라이언트들에게 메세지 전달
+//    @GetMapping(path = "/broadcast")
+//    public ApiResponse<String> boardcastTest(@RequestParam("message") String message){
+//        sseService.broadcast(message);
+//        log.info("Broadcasted completed");
+//        return ApiResponse.success(HttpStatus.OK, "Broadcast completed");
+//    }
+//
+//    //특정 클라이언트에게 메세지 전달
+//    @GetMapping(path = "/send")
+//    public ApiResponse<String> sendEvent(@RequestParam("email") String email, @RequestParam("message") String message){
+//
+//        sseService.sendEvent(email, message);
+//
+//        return ApiResponse.success(HttpStatus.OK, "Send completed");
+//    }
+//
+//    //모든 클라이언트의 이메일 확인(확인용)
+//    @GetMapping(path = "/getEmitters")
+//    public ApiResponse<String> getAllClientId(){
+//
+//        List<String> allClientEmail = sseService.getAllClientEmail();
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (String email : allClientEmail) {
+//            sb.append(email).append("\n");
+//        }
+//        return ApiResponse.success(HttpStatus.OK, sb.toString());
+//    }
 
 }
