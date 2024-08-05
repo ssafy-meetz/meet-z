25d593dd24920f9a89a7b5103ba69f20679f8585
손다인 <dain0826@naver.com>
2024-08-06 04:26:24 +0900

    [BE] chore: FanResponseDto -> FanInfo로 수정 (#177)

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/FanInfo.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/FanInfo.java
@@ -2,14 +2,14 @@ package com.c108.meetz.dto.response;
 
 import com.c108.meetz.domain.User;
 
-public record FanResponseDto(
+public record FanInfo(
         int userId,
         String name,
         String email,
         String phone
 ) {
 
-    public static FanResponseDto from(User user) {
-        return new FanResponseDto(user.getUserId(), user.getName(), user.getOriginEmail(), user.getPhone());
+    public static FanInfo from(User user) {
+        return new FanInfo(user.getUserId(), user.getName(), user.getOriginEmail(), user.getPhone());
     }
 }
