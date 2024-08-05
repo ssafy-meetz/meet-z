1f09c066733060739f29fb47eebb23918e15a84f
손다인 <dain0826@naver.com>
2024-08-06 04:24:33 +0900

    [BE] chore: chatDto -> chatInfo로 수정 (#177)

--- meetz-back/meetz/src/main/java/com/c108/meetz/api/ChatApi.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/api/ChatApi.java
@@ -1,19 +1,12 @@
 package com.c108.meetz.api;
 
-import com.c108.meetz.dto.ApiResponse;
-import com.c108.meetz.dto.request.ChatDto;
-import com.c108.meetz.dto.response.ChatListResponseDto;
-import com.c108.meetz.service.ChatRoomService;
+import com.c108.meetz.dto.request.ChatInfo;
 import com.c108.meetz.service.ChatService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import static org.springframework.http.HttpStatus.OK;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,7 +16,7 @@ public class ChatApi {
     private final ChatService chatService;
 
     @MessageMapping("/api/chat")
-    public void chat(ChatDto chat) {
+    public void chat(ChatInfo chat) {
         sendingOperations.convertAndSend("/sub/chatRoom/" + chat.getChatRoomId(), chat);
         chatService.saveChat(chat);
     }
