32223dfa12c650b8b147e9f47d6f6c1f87df292f
손다인 <dain0826@naver.com>
2024-08-06 21:32:06 +0900

    [BE] feat: chatInfo에 senderRole추가 (#190)

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/request/ChatInfo.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/request/ChatInfo.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class ChatInfo {
     private int chatRoomId; //채팅방 아이디
+    private String senderRole;
+    private int senderId;
     private int receiverId;
     private String content; //메시지 내용
 }
