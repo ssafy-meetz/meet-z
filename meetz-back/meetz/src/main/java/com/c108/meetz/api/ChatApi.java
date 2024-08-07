95445c292d02f8df2a28095705c5c44ca5811000
손다인 <dain0826@naver.com>
2024-08-07 12:14:32 +0900

    [BE] feat: 메세지 수신시 시간 저장 처리 (#190)

--- meetz-back/meetz/src/main/java/com/c108/meetz/api/ChatApi.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/api/ChatApi.java
@@ -1,10 +1,8 @@
 package com.c108.meetz.api;
 
+import com.c108.meetz.domain.Chat;
 import com.c108.meetz.dto.request.ChatInfo;
-import com.c108.meetz.repository.ManagerRepository;
-import com.c108.meetz.repository.UserRepository;
 import com.c108.meetz.service.ChatService;
-import com.c108.meetz.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -16,25 +14,14 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 public class ChatApi {
 
     private final SimpMessageSendingOperations sendingOperations;
-    private final UserRepository userRepository;
-    private final ManagerRepository managerRepository;
+
     private final ChatService chatService;
 
     @MessageMapping("/api/chat")
-    public void chat(ChatInfo chat) {
-        String role = chat.getSenderRole();
-        String email = SecurityUtil.getCurrentUserEmail();
-        int id =0;
-        if(role.equals("FAN")) {
-            id = userRepository.findByEmail(email).get().getUserId();
-        }
-        else if(role.equals("MANAGER")){
-            id = managerRepository.findByEmail(email).get().getManagerId();
-        }
-        chat.setSenderId(id);
-        chatService.saveChat(chat);
-        sendingOperations.convertAndSend("/sub/chatRoom/" + chat.getChatRoomId(), chat);
-
+    public void chat(ChatInfo chatInfo) {
+        Chat chat = chatService.saveChat(chatInfo);
+        chatInfo = ChatInfo.from(chat);
+        sendingOperations.convertAndSend("/sub/chatRoom/" + chatInfo.getChatRoomId(), chatInfo);
     }
 
 }
