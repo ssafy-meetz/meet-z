e798aefeda11d4610325dea4ec6ec7054dc69678
monam2 <kangcw0107@gmail.com>
2024-08-07 17:56:21 +0900

    [FE] feat: 팬측 채팅 내역 API 연결(199)

--- meetz-front/src/page/sessionContainer/setting/pages/FanSettingPage.tsx
+++ meetz-front/src/page/sessionContainer/setting/pages/FanSettingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SetCam from "../components/SetCam";
 import SetMemo from "../components/SetMemo";
 import SetNickname from "../components/SetNickname";
@@ -8,11 +8,28 @@ import StepBox from "../components/StepBox";
 import logo from "/src/assets/images/logo.png";
 import useEnvSettingStore from "../../../../zustand/useEnvSettingStore";
 import useCheckAuth from "../../../../hooks/meeting/useCheckAuth";
+import { messageDto } from "../../../../types/types";
+import getChatDetailForFan from "../../../../apis/managerChat/getChatDetailForFan";
+import fetchUserData from "../../../../lib/fetchUserData";
 
 const FanSettingPage: React.FC = () => {
   useCheckAuth("FAN");
+  const { accessToken } = fetchUserData();
   const { currentStep, isChattingBoxVisible, toggleChattingBox } =
     useEnvSettingStore();
+  const [meetingName, setMeetingName] = useState("");
+  const [fanId, setFanId] = useState(0);
+  const [managerId, setManagerId] = useState(0);
+
+  const mi: string | null = window.sessionStorage.getItem('mi');
+
+  const [chatHistory, setChatHistory] = useState<messageDto[]>([
+    // {
+    //   message: '안녕하세요, 미쯔팀입니다 어떤 문의 사항 이신가요?',
+    //   sender: 'agent',
+    //   chatRoomId: 
+    // },
+  ]);
 
   const steps = [
     { step: 1, label: "닉네임 설정" },
@@ -21,18 +38,39 @@ const FanSettingPage: React.FC = () => {
     { step: 4, label: "대기" },
   ];
 
+  const fetchChatHistory = async () => {
+    try {
+      const { chats, fanId, managerId } = await getChatDetailForFan(accessToken || "");
+      setChatHistory(chats);
+      setFanId(fanId);
+      setManagerId(managerId);
+      console.log(chats)
+    } catch (error) {
+      return;
+    }
+  };
+
+  useEffect(() => {
+    if (mi) {
+      const data = JSON.parse(mi);
+      setMeetingName(data.meetingName)
+    }
+
+    fetchChatHistory();
+  }, [])
+
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-[#f9f9f9]">
       <div className="max-w-screen-xl w-screen px-24 ">
         <div className="flex justify-between items-end font-medium text-xl mb-2">
           <img src={logo} alt="logo" className="w-[169px] h-[32px]" />
           <span className="text-[#ff7777] font-semibold text-shadow-shine cursor-default">
-            우주 최강 이승원 앨범 출시 팬 싸인회
+            {meetingName}
           </span>
         </div>
         <div className="flex border-2 rounded-3xl border-[#d9d9d9] bg-white shadow-2xl">
           {isChattingBoxVisible ? (
-            <ChattingBox />
+            <ChattingBox chatHistory={chatHistory} setChatHistory={setChatHistory} />
           ) : (
             <StepBox
               steps={steps}
