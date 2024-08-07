df65681b1cd78a8bab8f54e31f9dfedcfc9ab772
신민경 <tlsalsrud252@naver.com>
2024-08-07 12:00:32 +0900

    [FE] refactor: StarSessionContainerPage 추가

---
+++ meetz-front/src/page/sessionContainer/StarSessionContainerPage.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { useSessionStore } from "../../zustand/useSessionStore";
+import SessionSwitchPage from "./session/pages/SessionSwitchPage";
+import StarSessionPage from "./session/pages/StarSessionPage";
+import StarLoadingPage from "./setting/pages/StarLoadingPage";
+import fetchUserData from "../../lib/fetchUserData";
+import { EventSourcePolyfill } from "event-source-polyfill";
+
+type SessionInfo = {
+  fanId: string;
+  timer: number;
+  starName: string;
+  token: string;
+};
+const StarSessionContainerPage = () => {
+  const [remain, setRemain] = useState(200);
+  const { settingDone, setTimer, setStartName, setToken, setFanId } =
+    useSessionStore();
+
+  useEffect(() => {
+    fetchSSE();
+  }, []);
+
+  const setInfo = (info: SessionInfo) => {
+    return new Promise<void>((resolve) => {
+      setTimer(info.timer);
+      setStartName(info.starName);
+      setFanId(info.fanId);
+      setToken(info.token);
+      resolve();
+    });
+  };
+  const fetchSSE = () => {
+    console.log("SSE 연결 시도");
+    const { accessToken } = fetchUserData();
+    const eventSource = new EventSourcePolyfill(
+      `${import.meta.env.VITE_API_DEPLOYED_URL}/api/sessions/sse`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          Accept: "text/event-stream",
+        },
+        heartbeatTimeout: 7200 * 1000,
+      }
+    );
+    eventSource.onopen = () => {
+      console.log("!SSE 연결 성공!");
+    };
+    eventSource.onmessage = async (e: any) => {
+      const res = await e.data;
+      const parseData = JSON.parse(res);
+      console.log(parseData);
+      const info: SessionInfo = {
+        fanId: parseData.fanId,
+        timer: parseData.timer,
+        starName: parseData.starName,
+        token: parseData.token,
+      };
+      setInfo(info);
+      setRemain(parseData.remain);
+      eventSource.onerror = (e: any) => {
+        eventSource.close();
+        if (e.error) {
+          console.error(e.error);
+        }
+        if (e.target.readyState === EventSourcePolyfill.CLOSED) {
+          console.log("EventSourcePolyFill-CLOSED");
+        }
+      };
+    };
+  };
+
+  if (settingDone) {
+    return <StarSessionPage />;
+  }
+  if (remain === 0) {
+    return <SessionSwitchPage />;
+  }
+  return <StarLoadingPage />;
+};
+export default StarSessionContainerPage;
