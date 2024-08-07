e6fc8d2a91d0247653106e4379681d8f7c886fe8
손다인 <dain0826@naver.com>
2024-08-07 17:21:56 +0900

    [BE] refacotr: 블랙리스트 조회시 이름과 전화번호로 조회(#197)

--- meetz-back/meetz/src/main/java/com/c108/meetz/repository/BlackListRepository.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/repository/BlackListRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BlackListRepository extends JpaRepository<BlackList, Integer> {
-    boolean existsByNameAndEmailAndPhoneAndManager_ManagerId(String name, String email, String phone, int managerId);
+    boolean existsByNameAndPhoneAndManager_ManagerId(String name,String phone, int managerId);
 
     List<BlackList> findByManager_ManagerId(int managerId);
 
