d6847bf3d2d43341a2d9253c2c711fff69626fb6
손다인 <dain0826@naver.com>
2024-08-07 12:14:08 +0900

    [BE] feat: 기존 ChatList-> ChatInfo 로 타입 통일(#190)

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/request/ChatInfo.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/request/ChatInfo.java
@@ -1,6 +1,9 @@
 package com.c108.meetz.dto.request;
 
+import com.c108.meetz.domain.Chat;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class ChatInfo {
@@ -9,4 +12,17 @@ public class ChatInfo {
     private int senderId;
     private int receiverId;
     private String content; //메시지 내용
+    LocalDateTime createdAt;
+
+    public static ChatInfo from(Chat chat){
+        ChatInfo chatInfo = new ChatInfo();
+        chatInfo.setChatRoomId(chat.getChatRoom().getChatRoomId());
+        chatInfo.setSenderRole(chat.getSenderRole());
+        chatInfo.setSenderId(chat.getSenderId());
+        chatInfo.setReceiverId(chat.getReceiverId());
+        chatInfo.setContent(chat.getContent());
+        chatInfo.setCreatedAt(chat.getCreatedAt());
+        return chatInfo;
+    }
 }
+
