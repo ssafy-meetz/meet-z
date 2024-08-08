685675ac17bfdede819a342745a5a6f60d53c228
서민수 <minsu9812@naver.com>
2024-08-08 17:37:37 +0900

    [FE] feat: 미팅일정 삭제 기능추가 (#213)

--- meetz-front/src/page/meeting/page/DetailPage.tsx
+++ meetz-front/src/page/meeting/page/DetailPage.tsx
@@ -2,7 +2,7 @@ import useCheckAuth from '../../../hooks/meeting/useCheckAuth';
 import SendEmailModal from '../components/Detail/SendEmailModal';
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import getMeetingDetail from '../../../apis/meeting/getMeetingDetail';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { MeetingDetailDto } from '../../../types/types';
 import DetailHeader from '../components/Detail/DetailHeader';
 import DetailRoomList from '../components/Detail/DetailRoomList';
@@ -12,11 +12,21 @@ import CompleteEmailModal from '../components/Detail/CompleteEmailModal';
 import { useDetailstore } from '../../../zustand/useDetailStore';
 import fetchUserData from '../../../lib/fetchUserData';
 import Loading from '../../../common/Loading';
+import DeleteMeetingCheckModal from '../components/Detail/DeleteMeetingCheckModal';
+import DeleteMeetingModal from '../components/Detail/DeleteMeetingModal';
 
 const DetailPage = () => {
   useCheckAuth('MANAGER');
+  const navigate = useNavigate();
   const { meetingId } = useParams();
-  const { sendModalOpend, modalStep, openMailModal } = useDetailstore();
+  const {
+    sendModalOpend,
+    modalStep,
+    openMailModal,
+    openDeleteModal,
+    isDeleteModalOpen,
+    isDeletedModalOpen,
+  } = useDetailstore();
   const { accessToken } = fetchUserData();
   const [meetingData, setMeetingData] = useState<MeetingDetailDto>();
   const ref = useRef<HTMLDivElement | null>(null);
@@ -35,7 +45,10 @@ const DetailPage = () => {
     }
 
     try {
-      const { data, code } = await getMeetingDetail(+meetingId, accessToken || "");
+      const { data, code } = await getMeetingDetail(
+        +meetingId,
+        accessToken || ''
+      );
       if (code === 200) {
         setMeetingData(data);
       }
@@ -44,7 +57,7 @@ const DetailPage = () => {
         alert(error.response.data.message);
       }
     }
-  }
+  };
 
   useLayoutEffect(() => {
     scrollToTop();
@@ -52,14 +65,17 @@ const DetailPage = () => {
 
   useEffect(() => {
     fetchMeetingData();
-  }, [])
+  }, []);
 
+  const cancelHandler = () => {
+    navigate(-1);
+  };
   if (!meetingData) {
     return (
       <div className='flex justify-center items-center w-full h-screen'>
         <Loading width={160} height={160} />
       </div>
-    )
+    );
   }
 
   return (
@@ -78,11 +94,32 @@ const DetailPage = () => {
             </button>
           </div>
           <CleanFanList fanList={meetingData?.fanList} />
+          <div className='flex justify-center gap-10 pt-20'>
+            <button
+              onClick={openDeleteModal}
+              className='w-32 h-14 duration-100 ease-in-out transform hover:scale-105 hover:bg-[#ff626f] transition font-semibold rounded-2xl text-white bg-[#ff4f5d]'
+            >
+              일정삭제
+            </button>
+            <button
+              onClick={cancelHandler}
+              className='w-32 h-14  font-semibold rounded-2xl text-[#ff4f5d] border border-[#ff4f5d] hover:border-[#FF4F5D] focus:outline-none focus:border-[#FF4F5D] transition duration-100 ease-in-out transform hover:bg-[#ff4f5d] hover:text-white hover:scale-105 bg-white'
+            >
+              돌아가기
+            </button>
+          </div>
         </div>
       </main>
-      {sendModalOpend && modalStep === 0 && <SendEmailModal meetingId={meetingData?.meetingId} />}
+      {sendModalOpend && modalStep === 0 && (
+        <SendEmailModal meetingId={meetingData?.meetingId} />
+      )}
       {sendModalOpend && modalStep === 1 && <LoadEmailModal />}
       {sendModalOpend && modalStep === 2 && <CompleteEmailModal />}
+
+      {isDeleteModalOpen && (
+        <DeleteMeetingCheckModal meetingId={meetingData?.meetingId} />
+      )}
+      {isDeletedModalOpen && <DeleteMeetingModal />}
     </div>
   );
 };
