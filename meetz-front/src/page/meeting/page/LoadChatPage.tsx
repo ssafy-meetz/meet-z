5dc5c8396fdddf246a1a6de18bdabc7e30a2b17a
monam2 <kangcw0107@gmail.com>
2024-08-09 02:10:45 +0900

    [FE] feat: 매니저 채팅 로드 페이지 라우트시 타이머 제거(#227)

--- meetz-front/src/page/meeting/page/LoadChatPage.tsx
+++ meetz-front/src/page/meeting/page/LoadChatPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Loading from "../../../common/Loading";
 
@@ -5,16 +6,20 @@ const LoadChatPage = () => {
   const { meetingId } = useParams();
   const navigate = useNavigate();
 
-  setTimeout(() => {
-    navigate(`/manage/${meetingId}`);
-  }, 1500);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate(`/manage/${meetingId}`);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [meetingId, navigate]);
 
   return (
     <div className="mt-36 flex flex-grow flex-col items-center justify-center gap-12">
       <span className="text-3xl font-semibold">잠시 후 미팅이 시작됩니다!</span>
       <Loading width={100} height={100} />
     </div>
-  )
-}
+  );
+};
 
 export default LoadChatPage;
