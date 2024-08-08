f86741840fb03c9a48c2b5acef1b5fb8a9b01a73
김태연 <kam_ta_yon@naver.com>
2024-08-08 21:53:38 +0900

    [BE]feat: Report.java 스타 미팅 신고-#209

--- meetz-back/meetz/src/main/java/com/c108/meetz/domain/Report.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/domain/Report.java
@@ -1,30 +1,46 @@
 package com.c108.meetz.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Report {
 
     @Id
+    @Column(name = "report_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long reportId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "meeting_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Meeting meeting;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ReportReason reason;
 
-    @Column(nullable = true) // filePath를 null 가능으로 설정
+    @Column(name = "file_path", nullable = true)
     private String filePath;
+
+    @Builder
+    public Report(Meeting meeting, User user, ReportReason reason, String filePath) {
+        this.meeting = meeting;
+        this.user = user;
+        this.reason = reason;
+        this.filePath = filePath;
+    }
 }
