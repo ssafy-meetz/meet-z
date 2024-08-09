e678740b5a11fe7a3c9b5613c402c6b6e54806e2
unknown <wlk256032@gmail.com>
2024-08-09 16:56:40 +0900

    [BE] feat: type 추가

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/StarSseResponseDto.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/StarSseResponseDto.java
@@ -1,21 +1,30 @@
 package com.c108.meetz.dto.response;
 
-public record StarSseResponseDto(
-        boolean isBreak,
+public record StarSseResponseDto( //type 1: 넘기기, 2: 쉬는시간, 3: 사진 찍기, 4: 미팅 끝
+        int type,
         int remainFanNum,
         String currentFanName,
         int currentFanId,
         int timer
 ){
     public StarSseResponseDto() {
-        this(false, 1, "mingyung", 15, 0);
+        this(1, 1, "mingyung", 15, 0);
+    }
+
+    public static StarSseResponseDto sendNext(int remainFanNum, String currentFanName, int currentFanId, int timer) {
+        return new StarSseResponseDto(1, remainFanNum, currentFanName, currentFanId, timer);
     }
 
     public static StarSseResponseDto endMeeting() {
-        return new StarSseResponseDto(false, -1, null, 0, 0);
+        return new StarSseResponseDto(4, -1, null, 0, 0);
     }
+
     public static StarSseResponseDto breakMeeting() {
-        return new StarSseResponseDto(true, 0, null, 0, 0);
+        return new StarSseResponseDto(2, 0, null, 0, 0);
+    }
+
+    public static StarSseResponseDto takePicture() {
+        return new StarSseResponseDto(3, 0, null, 0, 0);
     }
 
 }
