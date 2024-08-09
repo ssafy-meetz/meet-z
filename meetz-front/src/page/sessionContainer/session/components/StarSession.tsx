70b82a4b9800c12ae5e22b41f4a146f9876ddb47
신민경 <tlsalsrud252@naver.com>
2024-08-09 15:22:32 +0900

    [FE] feat: 스타 페이지-상대방 로딩바 추가

--- meetz-front/src/page/sessionContainer/session/components/StarSession.tsx
+++ meetz-front/src/page/sessionContainer/session/components/StarSession.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Publisher, Subscriber } from "openvidu-browser";
 import Video from "./Video";
 import { useSessionStore } from "../../../../zustand/useSessionStore";
+import Loading from "../../../../common/Loading";
 
 interface SessionProps {
   subscriber: Subscriber | null;
@@ -16,20 +17,24 @@ interface Memo {
 function StarSession({ subscriber, publisher }: SessionProps) {
   const { starName, fanName, fanId } = useSessionStore();
   return (
-    <div className="flex w-full">
+    <div className="flex w-full align-middle">
       <div className="relative w-1/2" id="meetingVideo-star">
         <Video streamManager={publisher} />
         <p className="absolute top-0 left-0 p-1 text-white bg-black bg-opacity-75 rounded">
           {starName}
         </p>
       </div>
-      <div className="relative w-1/2" id="meetingVideo-fan">
+      <div
+        className="flex flex-col relative w-1/2 justify-center"
+        id="meetingVideo-fan"
+      >
         {subscriber && <Video streamManager={subscriber} />}
         {subscriber && (
           <p className="absolute top-0 right-0 p-1 text-white bg-black bg-opacity-75 rounded">
             {fanName}
           </p>
         )}
+        {!subscriber && <Loading width={150} height={150} />}
       </div>
     </div>
   );
