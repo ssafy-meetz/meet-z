39273d82520a455e249b2890a8e867440d210d01
monam2 <kangcw0107@gmail.com>
2024-08-10 02:37:16 +0900

    [FE] refactor: timeOptions 변경으로 인한 수정 및 현재 시간 이전의 시간 선택 불가하도록 변경(#242)

--- meetz-front/src/page/meeting/components/CreateAndModify/SetTimeStartBox.tsx
+++ meetz-front/src/page/meeting/components/CreateAndModify/SetTimeStartBox.tsx
@@ -1,22 +1,45 @@
+import React, { useMemo } from 'react';
 import useMeetingTimeStore from '../../../../zustand/useMeetingTimeStore';
 import { timeOptions, customStyles } from './TimeOptions';
 import Select from 'react-select';
 
+type OptionDto = {
+  value: string;
+  label: string;
+  isDisabled?: boolean;
+}
+
 const SetTimeStartBox = () => {
-  const { selectedTime, setSelectedTime } = useMeetingTimeStore();
+  const { selectedDate, selectedTime, setSelectedTime } = useMeetingTimeStore();
+
+  const today = new Date();
+  const formattedToday = today.getFullYear() + '-' + String(today.getMonth() + 1).padStart(2, '0') + '-' + String(today.getDate()).padStart(2, '0');
+  const currentTime = today.getHours() + ':' + String(today.getMinutes()).padStart(2, '0');
+
+  const modifiedTimeOptions = useMemo(() => {
+    if (selectedDate === formattedToday) {
+      return timeOptions.map(option => ({
+        ...option,
+        isDisabled: option.value < currentTime // 현재 시간보다 이전 시간은 비활성화
+      }));
+    }
+    return timeOptions;
+  }, [selectedDate, formattedToday, currentTime]);
+
+  const handleChange = (option: OptionDto) => {
+    setSelectedTime(option);
+  }
 
   return (
     <div className='flex gap-14 py-5 border-b items-center'>
       <div className='w-40'>
-        <span className='text-xl text-[#3a3a3a] font-semibold'>
-          시작 시간
-        </span>
+        <span className='text-xl text-[#3a3a3a] font-semibold'>시작 시간</span>
       </div>
       <div>
         <Select
-          options={timeOptions}
+          options={modifiedTimeOptions}
           value={selectedTime}
-          onChange={(option) => setSelectedTime(option)}
+          onChange={(option) => option && handleChange(option)}
           styles={customStyles}
           className='text-xl w-40'
           placeholder='선택'
@@ -26,4 +49,4 @@ const SetTimeStartBox = () => {
   )
 }
 
-export default SetTimeStartBox
+export default SetTimeStartBox;
