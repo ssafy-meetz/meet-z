3048dfbac4b3d46947eeb3b5d566bdc7fc988326
taeyeon <kam_ta_yon@naver.com>
2024-08-09 12:18:48 +0900

    [BE]fix: ReportResponseDto.java - 스타 팬신고 기능(#231)

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/ReportResponseDto.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/ReportResponseDto.java
@@ -1,2 +1,28 @@
-package com.c108.meetz.dto.response;public class ReportResponseDto {
+package com.c108.meetz.dto.response;
+
+import com.c108.meetz.domain.Meeting;
+import com.c108.meetz.domain.Report;
+import com.c108.meetz.domain.User;
+import lombok.Data;
+
+@Data
+public class ReportResponseDto {
+    private int meetingId;
+    private String starEmail;
+    private int fanId;
+    private boolean isReported;
+    private boolean isProfanity;
+    private String filePath;
+
+    public Report toEntity(Meeting meeting, User star, User fan) {
+        Report report = new Report();
+        report.setMeeting(meeting);
+        report.setStar(star);
+        report.setFan(fan);
+        report.setReported(isReported);
+        report.setProfanity(isProfanity);
+        report.setFilePath(null);
+        return report;
+    }
+
 }
