e64181ef0ee0d490e48d6d322f913b092c3587db
unknown <wlk256032@gmail.com>
2024-08-09 10:53:28 +0900

    [BE] feat: isBreak값 추가

--- meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/StarSseResponseDto.java
+++ meetz-back/meetz/src/main/java/com/c108/meetz/dto/response/StarSseResponseDto.java
@@ -1,12 +1,21 @@
 package com.c108.meetz.dto.response;
 
 public record StarSseResponseDto(
+        boolean isBreak,
         int remainFanNum,
         String currentFanName,
         int currentFanId,
         int timer
 ){
     public StarSseResponseDto() {
-        this(1, "mingyung", 15, 0);
+        this(false, 1, "mingyung", 15, 0);
     }
+
+    public static StarSseResponseDto endMeeting() {
+        return new StarSseResponseDto(false, -1, null, 0, 0);
+    }
+    public static StarSseResponseDto breakMeeting() {
+        return new StarSseResponseDto(true, 0, null, 0, 0);
+    }
+
 }
