5143878a526adb665a6675f795e6608a28ef4c49
monam2 <kangcw0107@gmail.com>
2024-08-11 05:12:26 +0900

    [FE] feat: 신고 목록 조회 API 헤더 토큰 추가(#259)

--- meetz-front/src/apis/meeting/getReportedList.ts
+++ meetz-front/src/apis/meeting/getReportedList.ts
@@ -2,10 +2,14 @@ import instance from "../axios"
 
 const getReportedList = async (meetingId: number, accessToken: string) => {
   try {
-    const { data, status } = await instance.get(`/api/meeting/${meetingId}/report`);
+    const { data, status } = await instance.get(`/api/meeting/${meetingId}/report`, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
 
     if (status === 200 && data.code === 200) {
-      return data.data;
+      return data;
     }
   } catch (error: any) {
     if (error.status === 400) {
