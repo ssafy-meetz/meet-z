3f37aedb56b9603dc3a93a723a4d672813328bdd
monam2 <kangcw0107@gmail.com>
2024-08-11 05:13:38 +0900

    [FE] 신고 내역 페이지 리팩토링 및 API 연결(#259)

--- meetz-front/src/page/meeting/page/MonitorPage.tsx
+++ meetz-front/src/page/meeting/page/MonitorPage.tsx
@@ -1,34 +1,54 @@
 import useCheckAuth from '../../../hooks/meeting/useCheckAuth';
-import Accordion from '../components/Accordion';
 import WarningModal from '../components/WarningModal';
 import BlackModal from '../components/BlackModal';
 import { useMonitorStore } from '../../../zustand/useMonitorStore';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import fetchUserData from '../../../lib/fetchUserData';
+import getReportedList from '../../../apis/meeting/getReportedList';
+import MonitorHeader from '../components/Monitor/MonitorHeader';
+import { ReportsDto } from '../../../types/types';
+import MonitorList from '../components/Monitor/MonitorList';
+
+interface ReportedDataDto {
+  meetingDuration: string; //number형으로 변경해야함
+  meetingEnd: string;
+  meetingName: string;
+  meetingStart: string;
+  meetingTerm: string; // number형으로 변경해야함
+  totalParticipants: number;
+  reportCount: number;
+  reports: ReportsDto[];
+}
 
 const MonitoringPage = () => {
+  const { meetingId } = useParams();
   const { warnModalOpend, blackModalOpend } = useMonitorStore();
+  const [reportedData, setReportedData] = useState<ReportedDataDto | null>(null);
+  const { accessToken } = fetchUserData();
 
   useCheckAuth('MANAGER');
+
+  const fetchReportedData = async () => {
+    try {
+      const { data } = await getReportedList(+(meetingId || '0'), accessToken || "");
+      setReportedData(data);
+    } catch (eeror) {
+
+    }
+  }
+
+  useEffect(() => {
+    console.log(meetingId)
+    fetchReportedData();
+  }, [])
 
   return (
     <div className='flex flex-col items-center'>
       <div className='max-w-screen-xl w-screen  px-24'>
-        <header className='items-center flex flex-col py-20'>
-          <h1 className='text-4xl font-bold pb-14'>신고 당한 미팅</h1>
-          <h1 className='text-[28px] font-bold text-[#ff4f5d]'>
-            우주 최강 이승원 앨범 출시 팬싸인회
-          </h1>
-          <h2 className='text-[#7D7D7D] text-[24px]'>
-            2024.07.24 오후 4시 (50명 참여)
-          </h2>
-        </header>
-        <span className='text-2xl font-bold'>총 3 건</span>
-        <main className='py-7 pb-72'>
-          <div className='justify-center items-center '>
-            <Accordion title='1번 팬 - 창우강' />
-            <Accordion title='2번 팬 - 승원빈' />
-            <Accordion title='3번 팬 - 다이슨' />
-          </div>
-        </main>
+        <MonitorHeader reportedData={reportedData} />
+        <span className='text-2xl font-bold'>총 {reportedData?.reportCount} 건</span>
+        <MonitorList reports={reportedData && reportedData.reports || null} />
       </div>
       {warnModalOpend && <WarningModal />}
       {blackModalOpend && <BlackModal />}
