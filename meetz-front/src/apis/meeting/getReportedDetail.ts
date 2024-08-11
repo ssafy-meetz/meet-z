5fe69623d31a2e53a443257356d6a6f2a4700f6a
monam2 <kangcw0107@gmail.com>
2024-08-11 20:31:52 +0900

    [FE] feat: 신고 내역 상세 조회 API 토큰 추가 및 데이터 포맷 수정(#264)

--- meetz-front/src/apis/meeting/getReportedDetail.ts
+++ meetz-front/src/apis/meeting/getReportedDetail.ts
@@ -3,10 +3,15 @@ import instance from "../axios"
 const getReportedDetail = async (meetingId: number, reportId: number, accessToken: string) => {
 
   try {
-    const { data, status } = await instance.get(`/api/meeting/${meetingId}/report/${reportId}`);
-
+    const { data, status } = await instance.get(`/api/meeting/${meetingId}/report/${reportId}`, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+    console.log(data)
     if (status === 200 && data.code) {
-      return data.data
+
+      return data;
     }
   } catch (error: any) {
     if (error.status === 400) {
