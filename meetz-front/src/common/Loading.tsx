1242ab7cd0d45a47b73becb058b236d5faa5e5e6
monam2 <kangcw0107@gmail.com>
2024-08-13 02:22:17 +0900

    [FE] feat: 로딩 컴포넌트 width, height 기본값 설정 및 props 바인딩으로 크기 조절 추가(#283)

--- meetz-front/src/common/Loading.tsx
+++ meetz-front/src/common/Loading.tsx
@@ -1,6 +1,4 @@
-import { MoonLoader } from "react-spinners";
-
-const Loading = ({ width, height }: { width: number; height: number }) => {
+const Loading = ({ width = 200, height = 200 }: { width: number; height: number }) => {
   return (
     <div role="status" className="flex justify-center">
       <svg
@@ -9,7 +7,7 @@ const Loading = ({ width, height }: { width: number; height: number }) => {
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        style={{ width: `${200}px`, height: `${200}px`, opacity: 0.5 }}
+        style={{ width: `${width}px`, height: `${height}px`, opacity: 0.5 }}
       >
         <path
           d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
