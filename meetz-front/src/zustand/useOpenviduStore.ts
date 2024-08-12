bea34e8c98200f80832ac32fc250c7b4aef9d853
minggwen <tlsalsrud252@naver.com>
2024-08-13 02:55:16 +0900

    [FE] feat: 기존 세션 정보를 저장할 상태 추가

--- meetz-front/src/zustand/useOpenviduStore.ts
+++ meetz-front/src/zustand/useOpenviduStore.ts
@@ -6,30 +6,35 @@ import {
   Subscriber,
 } from "openvidu-browser";
 
-interface OpenviduStore {
-  session: OVSession | null;
-  subscriber: Subscriber | null;
-  publisher: Publisher | null;
-  sessionId: string | "";
-  OV: OpenVidu | null;
-  setSession: (session: OVSession | null) => void;
-  setSubscriber: (subscriber: Subscriber | null) => void;
-  setPublisher: (publisher: Publisher | null) => void;
-  setSessionId: (sessionId: string | "") => void;
-  setOV: (OV: OpenVidu | null) => void;
-}
 
-const useOpenviduStore = create<OpenviduStore>((set) => ({
-  session: null,
-  subscriber: null,
-  publisher: null,
-  sessionId: "",
-  OV: null,
-  setSession: (session: OVSession | null) => set({ session }),
-  setSubscriber: (subscriber: Subscriber | null) => set({ subscriber }),
-  setPublisher: (publisher: Publisher | null) => set({ publisher }),
-  setSessionId: (sessionId: string | "") => set({ sessionId }),
-  setOV: (OV: OpenVidu | null) => set({ OV }),
-}));
-
-export default useOpenviduStore;
+interface OpenviduState {
+    session: OVSession | null;
+    subscriber: Subscriber | null;
+    publisher: Publisher | null;
+    OV: OpenVidu | null;
+    sessionId: string;
+    exSession: OVSession | null;
+    setSession: (session: OVSession | null) => void;
+    setSubscriber: (subscriber: Subscriber | null) => void;
+    setPublisher: (publisher: Publisher | null) => void;
+    setOV: (ov: OpenVidu | null) => void;
+    setSessionId: (sessionId: string) => void;
+    setExSession: (exSession: OVSession | null) => void;
+  }
+  
+  const useOpenviduStore = create<OpenviduState>((set) => ({
+    session: null,
+    subscriber: null,
+    publisher: null,
+    OV: null,
+    sessionId: "",
+    exSession: null,
+    setSession: (session) => set({ session }),
+    setSubscriber: (subscriber) => set({ subscriber }),
+    setPublisher: (publisher) => set({ publisher }),
+    setOV: (OV) => set({ OV }),
+    setSessionId: (sessionId) => set({ sessionId }),
+    setExSession: (exSession) => set({ exSession }),
+  }));
+  
+  export default useOpenviduStore;
