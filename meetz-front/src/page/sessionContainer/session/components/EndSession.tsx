437f6943c467cbbacb683bd9f7b0be1dde8a4117
서민수 <minsu9812@naver.com>
2024-08-12 15:50:14 +0900

    [FE] fix: 팬 미팅종료 문구수정 (#279)

--- meetz-front/src/page/sessionContainer/session/components/EndSession.tsx
+++ meetz-front/src/page/sessionContainer/session/components/EndSession.tsx
@@ -3,7 +3,8 @@ import logo from '/src/assets/images/sessionlogo.png';
 const EndSession = () => {
   // 문구를 구성하는 각 문자에 고유한 애니메이션을 적용
   const endMessage = '미팅이 종료되었습니다.';
-  const emailMessage = '스타와 찍은 사진은 이메일로 전송되었습니다.';
+  const emailMessage =
+    '스타와 함께한 순간, 사진으로 이메일에 담겼을지도! 놓치지 말고 확인해 보세요.';
 
   // 각 문자를 span으로 감싸서 개별 애니메이션을 적용
   const renderStaggeredText = (text: string) => {
@@ -23,14 +24,14 @@ const EndSession = () => {
   };
 
   return (
-    <div className='flex flex-col items-center gap-7 p-10  min-h-screen justify-center'>
+    <div className='flex flex-col items-center gap-5 p-10  min-h-screen justify-center'>
       <img
         src={logo}
         alt='logo'
         className='w-[300px] h-[60px] animate-staggeredFadeIn'
       />
       <div className='flex flex-col items-center text-center'>
-        <div className='text-white font-medium text-4xl mb-2'>
+        <div className='text-white font-medium text-5xl mb-4'>
           {renderStaggeredText(endMessage)}
         </div>
         <div className='text-white font-light text-3xl'>
