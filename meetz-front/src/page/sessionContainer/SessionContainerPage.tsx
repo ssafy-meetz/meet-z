168b612b7a2c148738085d6811071e5f7d4986c1
monam2 <kangcw0107@gmail.com>
2024-08-12 14:15:49 +0900

    [FE] feat: 새로고침/페이지 이동 시(유저 정보가 없을 경우) 로그인 페이지로 리다이렉트(#277)

--- meetz-front/src/page/sessionContainer/SessionContainerPage.tsx
+++ meetz-front/src/page/sessionContainer/SessionContainerPage.tsx
@@ -1,12 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import FanSessionContainerPage from "./FanSessionContainerPage";
 import StarSessionContainerPage from "./StarSessionContainerPage";
+import { useEffect } from "react";
 const SessionContainerPage = () => {
+  const navigate = useNavigate();
+
   const storedRole: String | null = window.sessionStorage.getItem("rl");
+  //여기서 useCheckAuth는 StarSessionContainerPage와 FanSessionContainerPage에 각각 권한에 맞게 선언되어 있음.
+
+  useEffect(() => {
+    if (!storedRole || storedRole === '') {
+      navigate('/')
+    }
+  }, [storedRole])
+
   if (storedRole === "STAR") {
     return <StarSessionContainerPage />;
   }
   if (storedRole === "FAN") {
     return <FanSessionContainerPage />;
   }
+
 };
 export default SessionContainerPage;
