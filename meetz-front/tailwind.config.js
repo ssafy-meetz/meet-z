05035a73a7003f61c335c4c2f90c9d29ce675bf9
서민수 <minsu9812@naver.com>
2024-08-12 15:14:27 +0900

    [FE] feat: 애니메이션 추가 (#276)

--- meetz-front/tailwind.config.js
+++ meetz-front/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'audiowide': ['Audiowide', 'sans-serif'],
+      },
       textShadow: {
         'default': '0 2px 4px rgba(0, 0, 0, 0.1)',
         'lg': '0 8px 16px rgba(0, 0, 0, 0.2)',
@@ -21,11 +24,26 @@ export default {
           '0%': { opacity: 0, transform: 'translateY(20px)' },
           '100%': { opacity: 1, transform: 'translateY(0)' },
         },
+        slideInFromTop: {
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        slideInFromBottom: {
+          '0%': { transform: 'translateY(100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        floatIn: {
+          '0%': { transform: 'translateY(30px) scale(0.55)', opacity: '0' },
+          '100%': { transform: 'translateY(0) scale(1)', opacity: '1' },
+        },
       },
       animation: {
         slideIn: 'slideIn 0.5s forwards',
         staggeredFadeIn: 'fadeIn 0.6s ease-out forwards',
         fadeIn: 'fadeIn 0.5s ease-in-out',
+        slideInFromTop: 'slideInFromTop 0.5s ease-out forwards',
+        slideInFromBottom: 'slideInFromBottom 0.5s ease-out forwards',
+        floatIn: 'floatIn 1s ease-out forwards',
       },
     },
   },
