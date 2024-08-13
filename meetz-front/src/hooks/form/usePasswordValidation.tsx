976b4ff346c0f93c2c6fcb79c7522d035e7ed3de
monam2 <kangcw0107@gmail.com>
2024-08-13 10:08:50 +0900

    [FE] fix: 비밀번호 검증 정규식 수정(#289)

--- meetz-front/src/hooks/form/usePasswordValidation.tsx
+++ meetz-front/src/hooks/form/usePasswordValidation.tsx
@@ -7,7 +7,7 @@ const usePasswordValidation = () => {
   const [password, setPassword] = useState("");
   const [isValidPassword, setIsValidPassword] = useState(false);
 
-  const passwordRegex = /^(?=.*[A-Za-z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+  const passwordRegex = /^(?=.*[!@#$%^&*-?_]).{8,16}$/;
 
   const validatePassword = (password: string) => {
     return passwordRegex.test(password);
