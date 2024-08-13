f7f68b5d8a86264dcfa17ad439e87d55eba936bf
monam2 <kangcw0107@gmail.com>
2024-08-13 14:18:19 +0900

    [FE] feat: useRecoder 커스텀 훅 생성(#292)

---
+++ meetz-front/src/hooks/session/useRecorder.tsx
@@ -0,0 +1,60 @@
+import { useState, useRef } from 'react';
+import postCheckProfanity from '../../apis/session/checkProfanity';
+
+interface UseRecorderProps {
+  audioChunks: Blob[]; // 녹음된 파일 객체(바인너리)
+  isRecording: boolean; // 녹음 중 여부 boolean 변수
+  startRecording: () => Promise<void>; //음성 녹음 시작
+  stopRecording: () => void; // 음성 녹음 중단
+  sendRecording: (accessToken: string) => Promise<void>;  // 음성 전송
+}
+
+const useRecorder = (): UseRecorderProps => {
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+
+  const startRecording = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const recorder = new MediaRecorder(stream);
+
+    recorder.ondataavailable = (event: BlobEvent) => {
+      if (event.data.size > 0) {
+        setAudioChunks((prev) => [...prev, event.data]);
+      }
+    };
+
+    recorder.start();
+    mediaRecorderRef.current = recorder;
+    setIsRecording(true);
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+      setIsRecording(false);
+    }
+  };
+
+  const sendRecording = async (accessToken: string) => {
+    const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+    const formData = new FormData();
+    formData.append('file', audioBlob, 'recording.wav');
+
+    try {
+      await postCheckProfanity(accessToken, formData);
+    } catch (error) {
+      return;
+    }
+  };
+
+  return {
+    audioChunks,
+    isRecording,
+    startRecording,
+    stopRecording,
+    sendRecording,
+  };
+};
+
+export default useRecorder;
