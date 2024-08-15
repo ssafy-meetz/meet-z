f36c17fcdce10f0a72a35e155f12c88e34075ffb
monam2 <kangcw0107@gmail.com>
2024-08-16 03:05:07 +0900

    [FE] feat: 모니터 페이지 경고/블랙리스트 관련 전역 상태 선언(#306)

--- meetz-front/src/zustand/useMonitorStore.ts
+++ meetz-front/src/zustand/useMonitorStore.ts
@@ -8,6 +8,17 @@ interface monitorStore {
   blackModalOpend: boolean;
   selectedOption: string;
   inputText: string;
+  blackCompleteModalOpened: boolean;
+  warnCompleteModalOpened: boolean;
+  alreadyWarnedModalOpened: boolean;
+  warnedErrorMsg: string;
+  setWarnedErrorMsg: (msg: string) => void;
+  openAlreadyWarnedModalOpened: () => void;
+  closeAlreadyWarnedModalOpened: () => void;
+  openBlackCompleteModalOpened: () => void;
+  closeBlackCompleteModalOpened: () => void;
+  openWarnCompleteModalOpened: () => void;
+  closeWarnCompleteModalOpened: () => void;
   setInputText: (text: string) => void;
   setSelectedOption: (val: string) => void;
   setReportedUserId: (id: number) => void;
@@ -24,6 +35,17 @@ export const useMonitorStore = create<monitorStore>((set) => ({
   blackModalOpend: false,
   selectedOption: "",
   inputText: "",
+  blackCompleteModalOpened: false,
+  warnCompleteModalOpened: false,
+  alreadyWarnedModalOpened: false,
+  warnedErrorMsg: "",
+  setWarnedErrorMsg: (msg) => set({ warnedErrorMsg: msg }),
+  openAlreadyWarnedModalOpened: () => set({ alreadyWarnedModalOpened: true }),
+  closeAlreadyWarnedModalOpened: () => set({ alreadyWarnedModalOpened: false }),
+  openWarnCompleteModalOpened: () => set({ warnCompleteModalOpened: true }),
+  closeWarnCompleteModalOpened: () => set({ warnCompleteModalOpened: false }),
+  openBlackCompleteModalOpened: () => set({ blackCompleteModalOpened: true }),
+  closeBlackCompleteModalOpened: () => set({ blackCompleteModalOpened: false }),
   setInputText: (text) => set({ inputText: text }),
   setSelectedOption: (val) => set({ selectedOption: val }),
   setReportedUserId: (id) => set({ reportedUserId: id }),
